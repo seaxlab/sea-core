b525ab17f99a20e1b5329971b83273b8dee8a1f1
shi.pengyan <shipengyan@yuantutech.com>
2019-05-14 19:04:09 +0800

    1.4.0

---
+++ src/main/java/com/github/spy/sea/core/common/CoreConst.java
@@ -0,0 +1,16 @@
+package com.github.spy.sea.core.common;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 模块名
+ *
+ * @author spy
+ * @version 1.0 2019-05-14
+ * @since 1.0
+ */
+@Slf4j
+public class CoreConst {
+
+    public String SYS_OPERATOR = "SYS_OPERATOR";
+}
