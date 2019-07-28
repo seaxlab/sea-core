91870fdef55a212b610267330de95e6bf3beedf1
shi.pengyan <shipengyan@yuantutech.com>
2019-07-28 11:22:57 +0800

    spring handler

---
+++ sea-core-spring/src/main/java/com/github/spy/sea/core/spring/handler/EncryptResponseHandler.java
@@ -0,0 +1,33 @@
+package com.github.spy.sea.core.spring.handler;
+
+import com.github.spy.sea.core.model.EncryptResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.core.annotation.AnnotatedElementUtils;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+/**
+ * 模块
+ *
+ * @author spy
+ * @version 1.0 2019-07-28
+ * @since 1.0
+ */
+@Slf4j
+public class EncryptResponseHandler implements HandlerMethodReturnValueHandler {
+    @Override
+    public boolean supportsReturnType(MethodParameter returnType) {
+        return AnnotatedElementUtils.hasAnnotation(returnType.getContainingClass(), EncryptResponse.class)
+                || returnType.hasMethodAnnotation(EncryptResponse.class);
+    }
+
+    @Override
+    public void handleReturnValue(Object returnValue, MethodParameter returnType, ModelAndViewContainer mavContainer, NativeWebRequest webRequest) throws Exception {
+        log.info("EncryptResponseHandler handle return value.");
+
+        //TODO more
+        mavContainer.setRequestHandled(true);
+    }
+}
