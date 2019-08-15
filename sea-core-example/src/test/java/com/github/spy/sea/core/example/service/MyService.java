1c5745f0444c39f49f6fae222db154a6091748a8
shi.pengyan <shipengyan@yuantutech.com>
2019-08-15 17:18:00 +0800

    error log file

---
+++ sea-core-example/src/test/java/com/github/spy/sea/core/example/service/MyService.java
@@ -0,0 +1,21 @@
+package com.github.spy.sea.core.example.service;
+
+import com.github.spy.sea.core.example.AbstractSpringTest;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * module name
+ *
+ * @author spy
+ * @version 1.0 2019-08-15
+ * @since 1.0
+ */
+@Slf4j
+public class MyService extends AbstractSpringTest {
+
+    @Test
+    public void run17() throws Exception {
+        log.error("----");
+    }
+}
