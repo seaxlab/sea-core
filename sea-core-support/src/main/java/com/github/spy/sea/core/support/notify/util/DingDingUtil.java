6a46e8c0150d0da7dbaadb00690b20f48de7aa7d
shi.pengyan <shipengyan@yuantutech.com>
2019-12-25 15:12:47 +0800

    dingding

---
+++ sea-core-support/src/main/java/com/github/spy/sea/core/support/notify/util/DingDingUtil.java
@@ -0,0 +1,46 @@
+package com.github.spy.sea.core.support.notify.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.Base64;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.net.URLEncoder;
+
+/**
+ * module name
+ *
+ * @author spy
+ * @version 1.0 2019/12/25
+ * @since 1.0
+ */
+@Slf4j
+public final class DingDingUtil {
+
+    /**
+     * 签名方式url
+     */
+    public static final String URL_SIGN = "https://oapi.dingtalk.com/robot/send?access_token={}&timestamp={}&sign={}";
+
+    /**
+     * 生成签名
+     *
+     * @param timestamp
+     * @param secret
+     * @return
+     */
+    public static String getSign(Long timestamp, String secret) {
+        try {
+            String stringToSign = timestamp + "\n" + secret;
+            Mac mac = Mac.getInstance("HmacSHA256");
+            mac.init(new SecretKeySpec(secret.getBytes("UTF-8"), "HmacSHA256"));
+            byte[] signData = mac.doFinal(stringToSign.getBytes("UTF-8"));
+            return URLEncoder.encode(new String(Base64.encodeBase64(signData)), "UTF-8");
+
+        } catch (Exception e) {
+            log.error("make dingding sign error", e);
+        }
+
+        return null;
+    }
+}
--- sea-core-support/src/test/java/com/github/spy/sea/core/support/notify/NotifyManagerTest.java
+++ sea-core-support/src/test/java/com/github/spy/sea/core/support/notify/NotifyManagerTest.java
@@ -1,6 +1,8 @@
 package com.github.spy.sea.core.support.notify;
 
+import com.github.spy.sea.core.message.util.MessageUtil;
 import com.github.spy.sea.core.support.notify.manager.impl.DingDingNotifyManager;
+import com.github.spy.sea.core.support.notify.util.DingDingUtil;
 import com.github.spy.sea.core.test.AbstractCore5Test;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -17,13 +19,21 @@ public class NotifyManagerTest extends AbstractCore5Test {
 
     @Test
     public void run17() throws Exception {
-        String endpoint = "https://oapi.dingtalk.com/robot/send?access_token=4a7f096e3d230cdd4a468ae7549d452b65cf7b66b88bc8255ee5438a2f8b3732";
+
+        //签名方式
+        String accessToken = "96ad13be7b5e8fdd86f8b7c1c089b799127abf6e6c558458cce722187a0c4c1f";
+        Long timestamp = System.currentTimeMillis();
+        String secret = "SECb8d39ac9513cfd90d248c55e6bfbd830b02da2260b9da6a96a550932585ad9ed";
+
+        String url = MessageUtil.format(DingDingUtil.URL_SIGN, accessToken, timestamp.toString(), DingDingUtil.getSign(timestamp, secret));
 
 
         DingDingNotifyManager notifyManager = new DingDingNotifyManager();
 
-        notifyManager.setEndpoint(endpoint);
+        notifyManager.setEndpoint(url);
 
         notifyManager.send("1");
     }
+
+
 }
