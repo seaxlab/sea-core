5aa139d1fa46463ff74c8b5849258ca2208bbac2
shi.pengyan <shipengyan@yuantutech.com>
2020-05-11 11:48:39 +0800

    es

---
+++ sea-core-es/src/main/java/com/github/spy/sea/core/es/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * module name
+ *
+ * @author spy
+ * @version 1.0 2020/5/11
+ * @since 1.0
+ */
+package com.github.spy.sea.core.es;
---
+++ sea-core-es/src/test/java/com/github/spy/sea/core/es/EsTest.java
@@ -0,0 +1,20 @@
+package com.github.spy.sea.core.es;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * module name
+ *
+ * @author spy
+ * @version 1.0 2020/5/11
+ * @since 1.0
+ */
+@Slf4j
+public class EsTest {
+
+    @Test
+    public void run16() throws Exception {
+        log.info("abc");
+    }
+}
