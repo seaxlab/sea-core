0320470775a28bbb550d53b2b1a434684a59714d
shi.pengyan <shipengyan@yuantutech.com>
2020-09-11 09:00:37 +0800

    bean util deep copy

--- sea-core-basic/src/main/java/com/github/spy/sea/core/bean/util/BeanUtil.java
+++ sea-core-basic/src/main/java/com/github/spy/sea/core/bean/util/BeanUtil.java
@@ -3,6 +3,8 @@ package com.github.spy.sea.core.bean.util;
 import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
+
 /**
  * bean util
  *
@@ -27,6 +29,31 @@ public final class BeanUtil {
             return null;
         }
         return JSON.parseObject(JSON.toJSONString(object), object.getClass());
+    }
+
+    /**
+     * deep copy.
+     *
+     * @param obj
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> T deepCopy(Object obj, Class<T> clazz) {
+        String json = JSON.toJSONString(obj);
+        return JSON.parseObject(json, clazz);
+    }
+
+    /**
+     * deep copy list.
+     *
+     * @param obj
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> List<T> deepCopyList(List<?> obj, Class<T> clazz) {
+        return JSON.parseArray(JSON.toJSONString(obj), clazz);
     }
 
 }
