65356495cb325051773018ca6d8720f6f2a13687
shipengyan <shipengyan@yuantutech.com>
2020-10-10 09:20:16 +0800

    data stats

---
+++ sea-core-basic/src/main/java/com/github/spy/sea/core/perf/DataStats.java
@@ -0,0 +1,69 @@
+package com.github.spy.sea.core.perf;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * common data collector for <font color='red'> system level, this is very important </font>
+ *
+ * @author spy
+ * @version 1.0 2020/4/18
+ * @since 1.0
+ */
+@Slf4j
+public class DataStats {
+    private static DataStats current = null;
+    private Map<String, AtomicLong> cache = new ConcurrentHashMap<>();
+
+    private DataStats() {
+    }
+
+    public static DataStats currentStatsHolder() {
+        if (null == current) {
+            synchronized (DataStats.class) {
+                if (null == current) {
+                    current = new DataStats();
+                }
+            }
+        }
+        return current;
+    }
+
+    /**
+     * increment
+     *
+     * @param metric
+     */
+    public void count(String metric) {
+        try {
+            cache.putIfAbsent(metric, new AtomicLong());
+            cache.get(metric).incrementAndGet();
+        } catch (Exception e) {
+            // ignore
+        }
+    }
+
+    /**
+     * reset old and new one.
+     *
+     * @return old data stats holder.
+     */
+    public static synchronized DataStats getAndReset() {
+        DataStats tmp = new DataStats();
+        DataStats old = currentStatsHolder();
+        current = tmp;
+        return old;
+    }
+
+    /**
+     * get cache.
+     *
+     * @return
+     */
+    public Map<String, AtomicLong> getCache() {
+        return cache;
+    }
+}
