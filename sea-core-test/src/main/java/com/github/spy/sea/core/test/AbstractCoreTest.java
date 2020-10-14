a0e3a704d193ab58ea9b6f71a93975fcc452f533
shipengyan <shipengyan@yuantutech.com>
2020-10-14 16:25:51 +0800

    sea-core-test

--- sea-core-test/src/main/java/com/github/spy/sea/core/test/AbstractCoreTest.java
+++ sea-core-test/src/main/java/com/github/spy/sea/core/test/AbstractCoreTest.java
@@ -1,9 +1,12 @@
 package com.github.spy.sea.core.test;
 
+import com.google.common.base.Stopwatch;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Junit4 测试基类
@@ -16,13 +19,17 @@ public abstract class AbstractCoreTest {
 
     private Logger _log = LoggerFactory.getLogger(getClass());
 
+    private static Stopwatch _stopwatch;
+
     @BeforeClass
     public static void testBefore() {
+        _stopwatch = Stopwatch.createStarted();
         System.out.println("-------------------- test begin ------------------");
     }
 
     @AfterClass
     public static void testEnd() {
+        System.out.println("cost: " + _stopwatch.elapsed(TimeUnit.MILLISECONDS) + "ms");
         System.out.println("-------------------- test  end  ------------------");
     }
 
