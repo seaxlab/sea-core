27c84300515054007fa68f2ebb50316e95f3732d
shipengyan <shipengyan@yuantutech.com>
2020-11-10 08:59:32 +0800

    doc

---
+++ sea-core-basic/src/main/java/com/github/spy/sea/core/annotation/Important.java
@@ -0,0 +1,30 @@
+package com.github.spy.sea.core.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * module name
+ *
+ * @author spy
+ * @version 1.0 2020/11/9
+ * @since 1.0
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.FIELD, ElementType.METHOD})
+public @interface Important {
+
+    /**
+     * 重要内容
+     *
+     * @return
+     */
+    String value() default "";
+
+    /**
+     * 重要程度
+     *
+     * @return
+     */
+    int level() default 1;
+}
