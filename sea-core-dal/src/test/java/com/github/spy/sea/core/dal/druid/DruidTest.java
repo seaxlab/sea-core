e4cfe0c68baaef7519146eabed3703613614cdeb
shipengyan <shipengyanvip@163.com>
2020-12-02 09:52:28 +0800

    sea-core-dal

---
+++ sea-core-dal/src/test/java/com/github/spy/sea/core/dal/druid/DruidTest.java
@@ -0,0 +1,15 @@
+package com.github.spy.sea.core.dal.druid;
+
+import com.github.spy.sea.core.test.AbstractCoreTest;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * module name
+ *
+ * @author spy
+ * @version 1.0 2020/12/2
+ * @since 1.0
+ */
+@Slf4j
+public class DruidTest extends AbstractCoreTest {
+}
