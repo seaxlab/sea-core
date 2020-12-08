0739c3894756e7234131e7a008526261530fc0a1
shipengyan <shipengyanvip@163.com>
2020-12-08 15:54:42 +0800

    zookeeper

---
+++ sea-core-basic/src/main/java/com/github/spy/sea/core/util/ZookeeperUtil.java
@@ -0,0 +1,18 @@
+package com.github.spy.sea.core.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * zookeeper util
+ *
+ * @author spy
+ * @version 1.0 2020/12/3
+ * @since 1.0
+ */
+@Slf4j
+public final class ZookeeperUtil {
+
+    //TODO
+    //use curator
+
+}
