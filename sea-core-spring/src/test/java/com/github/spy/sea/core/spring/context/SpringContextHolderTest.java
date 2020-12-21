1ef3475e28c671465e4bc4790bb74de48377760e
shipengyan <shipengyanvip@163.com>
2020-12-21 10:47:54 +0800

    spring context holder

---
+++ sea-core-spring/src/test/java/com/github/spy/sea/core/spring/context/SpringContextHolderTest.java
@@ -0,0 +1,21 @@
+package com.github.spy.sea.core.spring.context;
+
+import com.github.spy.sea.core.spring.BaseSpringTest;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * module name
+ *
+ * @author spy
+ * @version 1.0 2020/12/21
+ * @since 1.0
+ */
+@Slf4j
+public class SpringContextHolderTest extends BaseSpringTest {
+
+    @Test
+    public void run17() throws Exception {
+
+    }
+}
