2fd90bba5f21a0a461696f420015b9e5485e4200
shipengyan <shipengyanvip@163.com>
2021-01-20 08:53:26 +0800

    snowflake id worker

---
+++ sea-core-basic/src/test/java/com/github/spy/sea/core/guava/FilesTest.java
@@ -0,0 +1,21 @@
+package com.github.spy.sea.core.guava;
+
+import com.github.spy.sea.core.BaseCoreTest;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * module name
+ *
+ * @author spy
+ * @version 1.0 2021/1/18
+ * @since 1.0
+ */
+@Slf4j
+public class FilesTest extends BaseCoreTest {
+
+    @Test
+    public void test17() throws Exception {
+
+    }
+}
