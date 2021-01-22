2c93a9e49e06847404868b5be184c3ebe9bc6028
shipengyan <shipengyanvip@163.com>
2021-01-22 17:47:12 +0800

    sea core web auto configuration

--- sea-core-example/src/main/java/com/github/spy/sea/core/example/config/WebConfig.java
+++ sea-core-example/src/main/java/com/github/spy/sea/core/example/config/WebConfig.java
@@ -1,9 +1,7 @@
 package com.github.spy.sea.core.example.config;
 
-import com.github.spy.sea.core.web.filter.SeaGlobalFilter;
 import com.github.spy.sea.core.web.servlet.WebApplicationListener;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,17 +15,17 @@ import org.springframework.context.annotation.Configuration;
 @Slf4j
 @Configuration
 public class WebConfig {
-    @Bean
-    public FilterRegistrationBean seaGlobalFilter() {
-        FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(new SeaGlobalFilter());
-        registration.addUrlPatterns("/api/*");
-        registration.addInitParameter("filter", "/api/,/restapi/"); //添加默认参数
-        registration.addInitParameter("noFilter", "/api/common/,/restapi/common/,/referralLogin/");
-        registration.setName("SeaGlobalFilter");
-//        registration.setOrder(1);//设置优先级
-        return registration;
-    }
+//    @Bean
+//    public FilterRegistrationBean seaGlobalFilter() {
+//        FilterRegistrationBean registration = new FilterRegistrationBean();
+//        registration.setFilter(new SeaGlobalFilter());
+//        registration.addUrlPatterns("/api/*");
+//        registration.addInitParameter("filter", "/api/,/restapi/"); //添加默认参数
+//        registration.addInitParameter("noFilter", "/api/common/,/restapi/common/,/referralLogin/");
+//        registration.setName("SeaGlobalFilter");
+////        registration.setOrder(1);//设置优先级
+//        return registration;
+//    }
 
     @Bean
     public WebApplicationListener webApplicationListener() {
