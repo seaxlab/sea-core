dbbaa7af65b5089608db9d21ddab4332cde64907
shipengyan <shipengyanvip@163.com>
2021-01-23 12:05:54 +0800

    mock server

---
+++ sea-core-test/src/test/java/com/github/spy/sea/core/test/mockserver/callback/TestExpectationCallback.java
@@ -0,0 +1,33 @@
+package com.github.spy.sea.core.test.mockserver.callback;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mockserver.mock.action.ExpectationResponseCallback;
+import org.mockserver.model.HttpRequest;
+import org.mockserver.model.HttpResponse;
+
+import static org.mockserver.model.HttpResponse.notFoundResponse;
+import static org.mockserver.model.HttpResponse.response;
+
+/**
+ * module name
+ *
+ * @author spy
+ * @version 1.0 2021/1/23
+ * @since 1.0
+ */
+@Slf4j
+public class TestExpectationCallback implements ExpectationResponseCallback {
+
+    public HttpResponse handle(HttpRequest httpRequest) {
+        log.info("----callback handle...");
+        if (httpRequest.getPath().getValue().endsWith("/callback")) {
+            return httpResponse;
+        } else {
+            return notFoundResponse();
+        }
+    }
+
+    public static HttpResponse httpResponse = response()
+            .withStatusCode(200)
+            .withBody("success.");
+}
---
+++ sea-core-test/src/test/java/com/github/spy/sea/core/test/mockserver/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * module name
+ *
+ * @author spy
+ * @version 1.0 2021/1/23
+ * @since 1.0
+ */
+package com.github.spy.sea.core.test.mockserver;
