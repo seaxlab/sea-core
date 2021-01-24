c16b15a6667b2ebb5bb374d4502be92b48b183ab
shipengyan <shipengyanvip@163.com>
2021-01-24 17:59:45 +0800

    spring boot/cloud test

---
+++ sea-core-test/src/main/java/com/github/spy/sea/core/test/AbstractCoreSpringBootTest.java
@@ -0,0 +1,23 @@
+package com.github.spy.sea.core.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * module name
+ *
+ * @author spy
+ * @version 1.0 2021/1/24
+ * @since 1.0
+ */
+@Slf4j
+@RunWith(SpringJUnit4ClassRunner.class)
+// classes 使用测试类定义的
+//@SpringBootTest(classes = SpringBootWebApplication.class, webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestPropertySource(locations = "classpath:application.yml")
+public class AbstractCoreSpringBootTest {
+}
