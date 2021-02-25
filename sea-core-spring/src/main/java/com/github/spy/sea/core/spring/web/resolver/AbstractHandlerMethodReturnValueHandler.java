c65571605295e86771f730fc7ba3ceb1c37a9bfc
shipengyan <shipengyanvip@163.com>
2021-02-25 08:34:58 +0800

    handler method argument/ return value

---
+++ sea-core-spring/src/main/java/com/github/spy/sea/core/spring/web/resolver/AbstractHandlerMethodReturnValueHandler.java
@@ -0,0 +1,27 @@
+package com.github.spy.sea.core.spring.web.resolver;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+/**
+ * 处理指定返回值类型，可以对值进行增强
+ *
+ * @author spy
+ * @version 1.0 2021/2/25
+ * @since 1.0
+ */
+@Slf4j
+public class AbstractHandlerMethodReturnValueHandler implements HandlerMethodReturnValueHandler {
+    @Override
+    public boolean supportsReturnType(MethodParameter returnType) {
+        return false;
+    }
+
+    @Override
+    public void handleReturnValue(Object returnValue, MethodParameter returnType, ModelAndViewContainer mavContainer, NativeWebRequest webRequest) throws Exception {
+
+    }
+}
