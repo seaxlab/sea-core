70cd619e49d4bcf94900b9a578823660b3d6d707
shipengyan <shipengyanvip@163.com>
2021-03-03 09:28:01 +0800

    base info

--- sea-core-basic/src/main/java/com/github/spy/sea/core/common/CharConst.java
+++ sea-core-basic/src/main/java/com/github/spy/sea/core/common/CharConst.java
@@ -28,6 +28,46 @@ public class CharConst {
     public static final char SINGLE_QUOTE = '\'';
     public static final char AMP = '&';
 
+    public static final char UPPER_A = 'A';
+    public static final char LOWER_A = 'a';
+    public static final char UPPER_Z = 'Z';
+    public static final char LOWER_Z = 'z';
+    public static final char AT = '@';
+    public static final char LEFT_BRACE = '{';
+    public static final char RIGHT_BRACE = '}';
+    public static final char LEFT_BRACKET = '(';
+    public static final char RIGHT_BRACKET = ')';
+    public static final char DASH = '-';
+    public static final char PERCENT = '%';
+    public static final char PIPE = '|';
+    public static final char PLUS = '+';
+    public static final char QUESTION_MARK = '?';
+    public static final char EXCLAMATION_MARK = '!';
+    public static final char EQUALS = '=';
+    public static final char AMPERSAND = '&';
+    public static final char ASTERISK = '*';
+    public static final char STAR = ASTERISK;
+    public static final char BACK_SLASH = '\\';
+    public static final char DOLLAR = '$';
+    public static final char HASH = '#';
+    public static final char HAT = '^';
+    public static final char LEFT_CHEV = '<';
+    public static final char NEWLINE = '\n';
+    public static final char N = 'n';
+    public static final char Y = 'y';
+    public static final char QUOTE = '\"';
+    public static final char RETURN = '\r';
+    public static final char RIGHT_CHEV = '>';
+    public static final char SEMICOLON = ';';
+    public static final char BACKTICK = '`';
+    public static final char TILDA = '~';
+    public static final char LEFT_SQ_BRACKET = '[';
+    public static final char RIGHT_SQ_BRACKET = ']';
+    public static final char UNDERSCORE = '_';
+    public static final char ONE = '1';
+    public static final char ZERO = '0';
+
+
     private CharConst() {
     }
 
---
+++ sea-core-basic/src/main/java/com/github/spy/sea/core/component/function/CheckedFunction.java
@@ -0,0 +1,26 @@
+package com.github.spy.sea.core.component.function;
+
+import javax.annotation.Nullable;
+import java.io.Serializable;
+
+/**
+ * <pre>
+ *  https://gitee.com/596392912/mica/blob/master/mica-core/src/main/java/net/dreamlu/mica/core/function/CheckedCallable.java
+ * </pre>
+ *
+ * @author spy
+ * @version 1.0 2021/3/3
+ * @since 1.0
+ */
+@FunctionalInterface
+public interface CheckedFunction<T, R> extends Serializable {
+    /**
+     * Run the Function
+     *
+     * @param t T
+     * @return R R
+     * @throws Throwable CheckedException
+     */
+    @Nullable
+    R apply(@Nullable T t) throws Throwable;
+}
