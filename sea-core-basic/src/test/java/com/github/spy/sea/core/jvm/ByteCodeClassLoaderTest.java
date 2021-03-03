fbfdf2a5700ff085d464deccc87875093e10c8db
shipengyan <shipengyanvip@163.com>
2021-03-03 09:27:37 +0800

    memory java compiler

---
+++ sea-core-basic/src/test/java/com/github/spy/sea/core/jvm/ByteCodeClassLoaderTest.java
@@ -0,0 +1,29 @@
+package com.github.spy.sea.core.jvm;
+
+import com.github.spy.sea.core.BaseCoreTest;
+import com.github.spy.sea.core.jvm.classloader.ByteCodeClassLoader;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * module name
+ *
+ * @author spy
+ * @version 1.0 2021/3/3
+ * @since 1.0
+ */
+@Slf4j
+public class ByteCodeClassLoaderTest extends BaseCoreTest {
+    @Test
+    public void test16() throws Exception {
+        String className = "Foo";
+        String sourceCode = "public class " + className + " {" +
+                "    public void bar() {" +
+                "        System.out.println(\"Hello from bar !\");" +
+                "    }" +
+                "}";
+        Class<?> clazz = ByteCodeClassLoader.load(className, sourceCode);
+        Assert.assertEquals(className, clazz.getName());
+    }
+}
