b9e42addb29858506ef78d99eb24ab128461f330
shipengyan <shipengyanvip@163.com>
2021-03-19 09:45:24 +0800

    influxdb

---
+++ sea-core-dal/src/main/java/com/github/spy/sea/core/dal/influx/model/Table.java
@@ -0,0 +1,15 @@
+package com.github.spy.sea.core.dal.influx.model;
+
+import lombok.Data;
+
+/**
+ * module name
+ *
+ * @author spy
+ * @version 1.0 2021/3/18
+ * @since 1.0
+ */
+@Data
+public class Table {
+    private String name;
+}
