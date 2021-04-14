d001072236010d234a858c1f16186abc83386b06
shipengyan <shipengyanvip@163.com>
2021-04-14 15:49:36 +0800

    spring expression util

---
+++ sea-core-spring/src/test/java/com/github/spy/sea/core/spring/util/SpringExpressionUtilTest.java
@@ -0,0 +1,32 @@
+package com.github.spy.sea.core.spring.util;
+
+import com.github.spy.sea.core.spring.BaseSpringTest;
+import com.github.spy.sea.core.spring.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * module name
+ *
+ * @author spy
+ * @version 1.0 2021/4/14
+ * @since 1.0
+ */
+@Slf4j
+public class SpringExpressionUtilTest extends BaseSpringTest {
+
+    @Test
+    public void testSimple() throws Exception {
+        String content = SpringExpressionUtil.parse("name", "test", "#name", String.class);
+        log.info("content={}", content);
+    }
+
+    @Test
+    public void testComplexObj() throws Exception {
+        User user = new User();
+        user.setName("abc");
+
+        String content = SpringExpressionUtil.parse("user", user, "#user.name", String.class);
+        log.info("content={}", content);
+    }
+}
