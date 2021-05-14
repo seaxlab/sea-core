eafa4c5231bd55ff84bcc4873bf25b1d8e6680d9
shipengyan <shipengyanvip@163.com>
2021-05-14 20:07:03 +0800

    checker

---
+++ sea-core-model/src/main/java/com/github/spy/sea/core/checker/Checker.java
@@ -0,0 +1,29 @@
+package com.github.spy.sea.core.checker;
+
+import com.github.spy.sea.core.model.BaseResult;
+
+/**
+ * simple checker
+ *
+ * @author spy
+ * @version 1.0 2021/5/14
+ * @since 1.0
+ */
+public interface Checker<I, R> {
+
+
+    /**
+     * 校验器
+     *
+     * @param input
+     * @return
+     */
+    BaseResult<R> check(I input);
+
+    /**
+     * 多个checker时的执行顺序
+     */
+    default int order() {
+        return 0;
+    }
+}
