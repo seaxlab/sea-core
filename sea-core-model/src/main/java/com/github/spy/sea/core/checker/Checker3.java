ccc5a482806ede20db6adee626d894b86065b64c
shipengyan <shipengyanvip@163.com>
2021-05-19 08:48:57 +0800

    add checker2/3

---
+++ sea-core-model/src/main/java/com/github/spy/sea/core/checker/Checker3.java
@@ -0,0 +1,31 @@
+package com.github.spy.sea.core.checker;
+
+import com.github.spy.sea.core.model.BaseResult;
+
+/**
+ * simple checker
+ *
+ * @author spy
+ * @version 1.0 2021/5/14
+ * @since 1.0
+ */
+public interface Checker3<Input1, Input2, Input3, R> {
+
+
+    /**
+     * 校验器
+     *
+     * @param input1 input
+     * @param input2 input
+     * @param input3 input
+     * @return
+     */
+    BaseResult<R> check(Input1 input1, Input2 input2, Input3 input3);
+
+    /**
+     * 多个checker时的执行顺序
+     */
+    default int order() {
+        return 0;
+    }
+}
