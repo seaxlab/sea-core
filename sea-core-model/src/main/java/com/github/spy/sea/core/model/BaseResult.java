6c0e850bb5be3dde920c0032aa2db03cb45132b3
shipengyan <shipengyanvip@163.com>
2021-05-21 07:02:51 +0800

    base result/checker

--- sea-core-model/src/main/java/com/github/spy/sea/core/model/BaseResult.java
+++ sea-core-model/src/main/java/com/github/spy/sea/core/model/BaseResult.java
@@ -5,7 +5,6 @@ import com.alibaba.fastjson.annotation.JSONField;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import org.slf4j.helpers.FormattingTuple;
 import org.slf4j.helpers.MessageFormatter;
 
 import java.io.Serializable;
@@ -114,7 +113,7 @@ public class BaseResult<T> implements Serializable {
     /**
      * set value, mark success is true, clean error code and error message.
      *
-     * @param data
+     * @param data return obj
      */
     public void value(T data) {
         this.success = true;
@@ -141,15 +140,15 @@ public class BaseResult<T> implements Serializable {
         return new BaseResult(true, null);
     }
 
-    public static <T> BaseResult success(T data) {
-        return new BaseResult(true, null, data);
+    public static <T> BaseResult<T> success(T data) {
+        return new BaseResult<T>(true, null, data);
     }
 
     /**
      * success msg
      *
      * @param message msg
-     * @return
+     * @return base result
      */
     public static BaseResult successMsg(String message) {
         return new BaseResult(true, message);
@@ -160,7 +159,7 @@ public class BaseResult<T> implements Serializable {
      *
      * @param code         code
      * @param errorMessage msg
-     * @return
+     * @return base result
      */
     public static BaseResult successMsg(String code, String errorMessage) {
         return new BaseResult(true, code, errorMessage);
@@ -195,21 +194,29 @@ public class BaseResult<T> implements Serializable {
      *      //大卫拯救地球，yeah!
      *  </pre>
      *
-     * @param format
-     * @param argArray
+     * @param format message pattern
+     * @param args   param
      */
-    public void setErrorMsg(String format, Object... argArray) {
-        FormattingTuple ft = MessageFormatter.arrayFormat(format, argArray);
-        this.setErrorMessage(ft.getMessage());
+    public void setErrorMsg(String format, Object... args) {
+        this.setErrorMessage(MessageFormatter.arrayFormat(format, args).getMessage());
     }
 
+    /**
+     * set format msg.
+     *
+     * @param format message pattern
+     * @param args   param
+     */
+    public void setMsgF(String format, Object... args) {
+        this.setErrorMessage(MessageFormatter.arrayFormat(format, args).getMessage());
+    }
 
     /**
      * check is ok.
      *
-     * @return
+     * @return true/false
      */
-    @JSONField(serialize = false)
+    @JSONField(serialize = false, deserialize = false)
     public boolean isOk() {
         if (success != null) {
             return success;
@@ -221,9 +228,9 @@ public class BaseResult<T> implements Serializable {
     /**
      * check is fail.
      *
-     * @return
+     * @return true/false
      */
-    @JSONField(serialize = false)
+    @JSONField(serialize = false, deserialize = false)
     public boolean isFail() {
         if (success != null) {
             return !success;
