5757a459288d183083d96e1cc4cd5dbb341be7d1
shipengyan <shipengyanvip@163.com>
2021-05-26 15:20:21 +0800

    base enum

--- sea-core-basic/src/main/java/com/github/spy/sea/core/enums/IBaseEnum2.java
+++ sea-core-basic/src/main/java/com/github/spy/sea/core/enums/IBaseEnum2.java
@@ -6,16 +6,24 @@ import java.io.Serializable;
  * 状态枚举标准定义
  *
  * @author spy
- * @version 1.0 2020/9/11
+ * @version 1.0 2021/5/26
  * @since 1.0
  */
-public interface IBaseEnum<T> extends Serializable {
+public interface IBaseEnum2<T> extends Serializable {
+
     /**
      * 标准编码
      *
      * @return
      */
     T getCode();
+
+    /**
+     * 对外名称
+     *
+     * @return
+     */
+    String getName();
 
     /**
      * 描述
