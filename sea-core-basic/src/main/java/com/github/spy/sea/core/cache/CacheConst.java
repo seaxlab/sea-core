459ff03f210ecd3dbca3632183ebfe745f0ae26f
shipengyan <shipengyanvip@163.com>
2021-06-01 13:06:12 +0800

    cache const

---
+++ sea-core-basic/src/main/java/com/github/spy/sea/core/cache/CacheConst.java
@@ -0,0 +1,24 @@
+package com.github.spy.sea.core.cache;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * cache const
+ *
+ * @author spy
+ * @version 1.0 2021/6/1
+ * @since 1.0
+ */
+@Slf4j
+public class CacheConst {
+
+    /**
+     * 空对象
+     */
+    public static final String EMPTY_OBJ = "{}";
+
+    /**
+     * 空数组
+     */
+    public static final String EMPTY_ARRAY = "[]";
+}
