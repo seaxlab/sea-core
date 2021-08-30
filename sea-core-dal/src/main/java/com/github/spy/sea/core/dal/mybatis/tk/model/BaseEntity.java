7dca6cd6d27b23d10ba3a91efc8bca071eb42f1b
shipengyan <shipengyanvip@163.com>
2021-08-30 08:59:27 +0800

    base entity in DAL

---
+++ sea-core-dal/src/main/java/com/github/spy/sea/core/dal/mybatis/tk/model/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.github.spy.sea.core.dal.mybatis.tk.model;
+
+import com.github.spy.sea.core.dal.mybatis.common.model.BaseEmptyEntity;
+import lombok.Data;
+
+import javax.persistence.Column;
+import java.util.Date;
+
+/**
+ * module name
+ *
+ * @author spy
+ * @version 1.0 2021/8/30
+ * @since 1.0
+ */
+@Data
+public class BaseEntity extends BaseEmptyEntity {
+
+    @Column(name = "`create_user`")
+    private String createUser;
+
+    @Column(name = "`create_time`")
+    private Date createTime;
+
+    @Column(name = "`update_user`")
+    private String updateUser;
+
+    @Column(name = "`update_time`")
+    private Date updateTime;
+}
