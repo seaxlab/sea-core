083b08361021636498d8229268b66e32070db646
shipengyan <shipengyanvip@163.com>
2021-09-01 08:58:23 +0800

    model const

--- sea-core-model/src/main/java/com/github/spy/sea/core/model/common/ModelConst.java
+++ sea-core-model/src/main/java/com/github/spy/sea/core/model/common/ModelConst.java
@@ -13,12 +13,41 @@ import lombok.extern.slf4j.Slf4j;
 public final class ModelConst {
 
     // field
+    public static final String id = "id";
+    public static final String CODE = "code";
+    public static final String GROUP_CODE = "groupCode";
+    public static final String PARENT_CODE = "parentCode";
+
+    public static final String NAME = "name";
+    public static final String TYPE = "type";
+    public static final String BIZ_TYPE = "bizType";
+    public static final String BIZ_SUB_TYPE = "bizSubType";
+    public static final String STATUS = "status";
+    public static final String VALUE = "value";
+
+    public static final String SORT = "sort";
+    public static final String PRIORITY = "priority";
+
     public static final String INTRO = "intro";
     public static final String EXTRA = "extra";
     public static final String REMARK = "remark";
+    public static final String DESCRIPTION = "description";
+
     public static final String IS_ENABLED = "isEnabled";
     public static final String IS_DELETED = "isDeleted";
+    public static final String VERSION = "version";
     public static final String TENANT_ID = "tenantId";
+
+    public static final String CREATOR = "creator";
+    public static final String CREATE_USER = "createUser";
+    public static final String CREATE_TIME = "createTime";
+    public static final String GMT_CREATE = "gmtCreate";
+
+    public static final String EDITOR = "editor";
+    public static final String UPDATE_USER = "updateUser";
+    public static final String UPDATE_TIME = "updateTime";
+    public static final String GMT_MODIFY = "gmtModify";
+
 
     // value
     public static final String REMARK_EMPTY = "-";
