20f85f04fa2ec842d6411368feff74bef04bb98c
shipengyan <shipengyanvip@163.com>
2021-09-01 08:49:16 +0800

    model const

---
+++ sea-core-model/src/main/java/com/github/spy/sea/core/model/common/ModelConst.java
@@ -0,0 +1,29 @@
+package com.github.spy.sea.core.model.common;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * module name
+ *
+ * @author spy
+ * @version 1.0 2021/9/1
+ * @since 1.0
+ */
+@Slf4j
+public final class ModelConst {
+
+    // field
+    public static final String INTRO = "intro";
+    public static final String EXTRA = "extra";
+    public static final String REMARK = "remark";
+    public static final String IS_ENABLED = "is_enabled";
+    public static final String IS_DELETED = "is_deleted";
+    public static final String TENANT_ID = "tenant_id";
+
+    // value
+    public static final String REMARK_EMPTY = "-";
+    public static final String ID_EMPTY = "0";
+    public static final Integer ID_INT_EMPTY = 0;
+    public static final Long ID_LONG_EMPTY = 0L;
+
+}
