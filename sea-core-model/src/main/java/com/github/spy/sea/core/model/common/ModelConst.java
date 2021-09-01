984021466e9547886586b819b736944a61123ee5
shipengyan <shipengyanvip@163.com>
2021-09-01 17:55:00 +0800

    model const

--- sea-core-model/src/main/java/com/github/spy/sea/core/model/common/ModelConst.java
+++ sea-core-model/src/main/java/com/github/spy/sea/core/model/common/ModelConst.java
@@ -14,6 +14,8 @@ public final class ModelConst {
 
     // field
     public static final String ID = "id";
+    public static final String REF_ID = "refId";
+
     public static final String CODE = "code";
     public static final String GROUP_CODE = "groupCode";
     public static final String PARENT_CODE = "parentCode";
@@ -27,12 +29,14 @@ public final class ModelConst {
     public static final String BIZ_SUB_TYPE_CODE = "bizSubTypeCode";
 
     public static final String STATUS = "status";
+    public static final String STATE = "state";
     public static final String VALUE = "value";
 
     public static final String SORT = "sort";
     public static final String ORDER = "order";
     public static final String PRIORITY = "priority";
 
+    public static final String PREFIX = "prefix";
     public static final String INTRO = "intro";
     public static final String EXTRA = "extra";
     public static final String REMARK = "remark";
@@ -56,6 +60,11 @@ public final class ModelConst {
     public static final String TRADING_DAY = "tradingDay";
     public static final String BILL_DATE = "billDate";
 
+    public static final String BEGIN_DATE = "beginDate";
+    public static final String END_DATE = "endDate";
+    public static final String BEGIN_TIME = "beginTime";
+    public static final String END_TIME = "endTime";
+
     public static final String PERIOD_TYPE = "periodType";
 
     public static final String BATCH_NO = "batchNo";
@@ -68,6 +77,11 @@ public final class ModelConst {
     public static final String TAG5 = "tag5";
     public static final String TAG6 = "tag6";
 
+    public static final String REQUEST_ID = "requestId";
+    public static final String TRACE_ID = "traceId";
+    public static final String SOURCE = "source";
+    public static final String CHANNEL = "channel";
+
 
     // value
     public static final String REMARK_EMPTY = "-";
@@ -75,4 +89,6 @@ public final class ModelConst {
     public static final Integer ID_INT_EMPTY = 0;
     public static final Long ID_LONG_EMPTY = 0L;
 
+    private ModelConst() {
+    }
 }
