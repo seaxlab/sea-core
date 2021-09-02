b7ee1cecec803ab41f4d4a08f4d9923b219c545b
shipengyan <shipengyanvip@163.com>
2021-09-02 10:16:13 +0800

    array util

--- sea-core-basic/src/main/java/com/github/spy/sea/core/util/ArrayUtil.java
+++ sea-core-basic/src/main/java/com/github/spy/sea/core/util/ArrayUtil.java
@@ -241,4 +241,169 @@ public final class ArrayUtil {
         array[i] = array[j];
         array[j] = t;
     }
+
+
+    /**
+     * 将新元素添加到已有数组中<br>
+     * 添加新元素会生成一个新的数组，不影响原数组
+     *
+     * @param <T>         数组元素类型
+     * @param buffer      已有数组
+     * @param newElements 新元素
+     * @return 新数组
+     */
+    @SafeVarargs
+    public static <T> T[] append(T[] buffer, T... newElements) {
+        if (isEmpty(buffer)) {
+            return newElements;
+        }
+        return insert(buffer, buffer.length, newElements);
+    }
+
+    /**
+     * 将新元素添加到已有数组中<br>
+     * 添加新元素会生成一个新的数组，不影响原数组
+     *
+     * @param <T>         数组元素类型
+     * @param array       已有数组
+     * @param newElements 新元素
+     * @return 新数组
+     */
+    @SafeVarargs
+    public static <T> Object append(Object array, T... newElements) {
+        if (isEmpty(array)) {
+            return newElements;
+        }
+        return insert(array, length(array), newElements);
+    }
+
+    /**
+     * 将元素值设置为数组的某个位置，当给定的index大于数组长度，则追加
+     *
+     * @param <T>    数组元素类型
+     * @param buffer 已有数组
+     * @param index  位置，大于长度追加，否则替换
+     * @param value  新值
+     * @return 新数组或原有数组
+     * @since 4.1.2
+     */
+    public static <T> T[] setOrAppend(T[] buffer, int index, T value) {
+        if (index < buffer.length) {
+            Array.set(buffer, index, value);
+            return buffer;
+        } else {
+            return append(buffer, value);
+        }
+    }
+
+    /**
+     * 将元素值设置为数组的某个位置，当给定的index大于数组长度，则追加
+     *
+     * @param array 已有数组
+     * @param index 位置，大于长度追加，否则替换
+     * @param value 新值
+     * @return 新数组或原有数组
+     * @since 4.1.2
+     */
+    public static Object setOrAppend(Object array, int index, Object value) {
+        if (index < length(array)) {
+            Array.set(array, index, value);
+            return array;
+        } else {
+            return append(array, value);
+        }
+    }
+
+    /**
+     * 将新元素插入到到已有数组中的某个位置<br>
+     * 添加新元素会生成一个新的数组，不影响原数组<br>
+     * 如果插入位置为为负数，从原数组从后向前计数，若大于原数组长度，则空白处用null填充
+     *
+     * @param <T>         数组元素类型
+     * @param buffer      已有数组
+     * @param index       插入位置，此位置为对应此位置元素之前的空档
+     * @param newElements 新元素
+     * @return 新数组
+     * @since 4.0.8
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T[] insert(T[] buffer, int index, T... newElements) {
+        return (T[]) insert((Object) buffer, index, newElements);
+    }
+
+    /**
+     * 将新元素插入到到已有数组中的某个位置<br>
+     * 添加新元素会生成一个新的数组，不影响原数组<br>
+     * 如果插入位置为为负数，从原数组从后向前计数，若大于原数组长度，则空白处用null填充
+     *
+     * @param <T>         数组元素类型
+     * @param array       已有数组
+     * @param index       插入位置，此位置为对应此位置元素之前的空档
+     * @param newElements 新元素
+     * @return 新数组
+     * @since 4.0.8
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> Object insert(Object array, int index, T... newElements) {
+        if (isEmpty(newElements)) {
+            return array;
+        }
+        if (isEmpty(array)) {
+            return newElements;
+        }
+
+        final int len = length(array);
+        if (index < 0) {
+            index = (index % len) + len;
+        }
+
+        final T[] result = newArray(array.getClass().getComponentType(), Math.max(len, index) + newElements.length);
+        System.arraycopy(array, 0, result, 0, Math.min(len, index));
+        System.arraycopy(newElements, 0, result, index, newElements.length);
+        if (index < len) {
+            System.arraycopy(array, index, result, index + newElements.length, len - index);
+        }
+        return result;
+    }
+
+
+    /**
+     * 获取数组长度<br>
+     * 如果参数为{@code null}，返回0
+     *
+     * <pre>
+     * ArrayUtil.length(null)            = 0
+     * ArrayUtil.length([])              = 0
+     * ArrayUtil.length([null])          = 1
+     * ArrayUtil.length([true, false])   = 2
+     * ArrayUtil.length([1, 2, 3])       = 3
+     * ArrayUtil.length(["a", "b", "c"]) = 3
+     * </pre>
+     *
+     * @param array 数组对象
+     * @return 数组长度
+     * @throws IllegalArgumentException 如果参数不为数组，抛出此异常
+     * @see Array#getLength(Object)
+     * @since 3.0.8
+     */
+    public static int length(Object array) throws IllegalArgumentException {
+        if (null == array) {
+            return 0;
+        }
+        return Array.getLength(array);
+    }
+
+
+    /**
+     * 新建一个空数组
+     *
+     * @param <T>           数组元素类型
+     * @param componentType 元素类型
+     * @param newSize       大小
+     * @return 空数组
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T[] newArray(Class<?> componentType, int newSize) {
+        return (T[]) Array.newInstance(componentType, newSize);
+    }
 }
