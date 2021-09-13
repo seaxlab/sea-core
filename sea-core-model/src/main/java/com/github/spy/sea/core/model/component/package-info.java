4877066529101352df5356f709f62f0656e0f499
shipengyan <shipengyanvip@163.com>
2021-09-13 13:50:07 +0800

    person model

---
+++ sea-core-model/src/main/java/com/github/spy/sea/core/model/component/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * module name
+ *
+ * @author spy
+ * @version 1.0 2021/9/9
+ * @since 1.0
+ */
+package com.github.spy.sea.core.model.component;
---
+++ sea-core-model/src/main/java/com/github/spy/sea/core/model/component/person/Age.java
@@ -0,0 +1,15 @@
+package com.github.spy.sea.core.model.component.person;
+
+import com.github.spy.sea.core.model.DTO;
+import lombok.Data;
+
+/**
+ * module name
+ *
+ * @author spy
+ * @version 1.0 2021/9/9
+ * @since 1.0
+ */
+@Data
+public class Age extends DTO {
+}
