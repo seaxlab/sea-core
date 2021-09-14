0bbfd29e45411727d3887e8e0554d3b1a0e4a43c
shipengyan <shipengyanvip@163.com>
2021-09-14 08:28:53 +0800

    address

--- sea-core-model/src/main/java/com/github/spy/sea/core/model/component/person/Address.java
+++ sea-core-model/src/main/java/com/github/spy/sea/core/model/component/person/Address.java
@@ -12,4 +12,53 @@ import lombok.Data;
  */
 @Data
 public class Address extends DTO {
+
+    // 省
+    private String provinceCode;
+    private String provinceName;
+
+    // 市
+    private String cityCode;
+    private String cityName;
+
+    // 区
+    private String countryCode;
+    private String countryName;
+
+    // 街道
+    private String streetCode;
+    private String streetName;
+
+    // 具体地址
+    private String detail;
+
+    /**
+     * 全名
+     *
+     * @return string
+     */
+    public String getFullAddress() {
+        String address = contact(provinceName)
+                + contact(cityName, "")
+                + contact(countryName, "")
+                + contact(streetName, "")
+                + contact(detail, "");
+        
+        return address.trim();
+    }
+
+    private String contact(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            return "";
+        }
+        return name;
+    }
+
+    private String contact(String name, String defaultVal) {
+        if (name == null || name.trim().isEmpty()) {
+            return defaultVal;
+        }
+        return name + " ";
+    }
+
 }
