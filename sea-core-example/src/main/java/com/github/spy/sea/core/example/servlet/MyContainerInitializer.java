6675b06378c2d561d2d6fd6e83028417bda20488
shipengyan <shipengyanvip@163.com>
2021-09-18 14:29:37 +0800

    ServletContainerInitializer

---
+++ sea-core-example/src/main/java/com/github/spy/sea/core/example/servlet/MyContainerInitializer.java
@@ -0,0 +1,16 @@
+package com.github.spy.sea.core.example.servlet;
+
+import javax.servlet.ServletContext;
+
+/**
+ * module name
+ *
+ * @author spy
+ * @version 1.0 2021/9/18
+ * @since 1.0
+ */
+public interface MyContainerInitializer {
+
+    void onStartup(ServletContext context);
+
+}
