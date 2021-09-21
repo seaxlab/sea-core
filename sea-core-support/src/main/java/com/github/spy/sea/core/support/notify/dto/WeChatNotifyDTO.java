0c0f9816c594a776410d475d9ee449691f290a42
shipengyan <shipengyanvip@163.com>
2021-09-21 20:18:50 +0800

    wechat notify

---
+++ sea-core-support/src/main/java/com/github/spy/sea/core/support/notify/dto/WeChatNotifyDTO.java
@@ -0,0 +1,14 @@
+package com.github.spy.sea.core.support.notify.dto;
+
+import lombok.Data;
+
+/**
+ * module name
+ *
+ * @author spy
+ * @version 1.0 2021/9/21
+ * @since 1.0
+ */
+@Data
+public class WeChatNotifyDTO extends BaseNotifyDTO {
+}
