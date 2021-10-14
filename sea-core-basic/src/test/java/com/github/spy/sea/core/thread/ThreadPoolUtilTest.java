bbac56090e8d7d109e9d44d10687bf7485fe1ff0
shipengyan <shipengyanvip@163.com>
2021-10-14 14:22:55 +0800

    test

--- sea-core-basic/src/test/java/com/github/spy/sea/core/thread/ThreadPoolUtilTest.java
+++ sea-core-basic/src/test/java/com/github/spy/sea/core/thread/ThreadPoolUtilTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * module name
@@ -40,4 +41,23 @@ public class ThreadPoolUtilTest extends BaseCoreTest {
         Object obj = CompletableFuture.allOf(f1, f2).get();
         log.info("obj={}", obj);
     }
+
+    @Test
+    public void testCreateTemp2() throws Exception {
+        // 单线程时，顺序执行
+        ThreadPoolExecutor tpe = ThreadPoolUtil.createTemp("test-pool", 1, 1);
+        log.info("start");
+        AtomicInteger count = new AtomicInteger(0);
+        for (int i = 0; i < 100; i++) {
+            String idx = "" + i;
+            tpe.submit(() -> {
+                log.info("count={},index={}", count.incrementAndGet(), idx);
+                sleep(3);
+            });
+        }
+
+
+        sleepMinute(5);
+    }
+
 }
