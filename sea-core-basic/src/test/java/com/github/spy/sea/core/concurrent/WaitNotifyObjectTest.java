069cd92ed04eabb4888d5abb058a3cdca981eaa0
shipengyan <shipengyanvip@163.com>
2021-10-28 08:59:14 +0800

    wait notify object

---
+++ sea-core-basic/src/test/java/com/github/spy/sea/core/concurrent/WaitNotifyObjectTest.java
@@ -0,0 +1,34 @@
+
+package com.github.spy.sea.core.concurrent;
+
+import com.github.spy.sea.core.BaseCoreTest;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Test;
+
+@Slf4j
+public class WaitNotifyObjectTest extends BaseCoreTest {
+
+    @Test
+    public void removeFromWaitingThreadTable() throws Exception {
+        final WaitNotifyObject waitNotifyObject = new WaitNotifyObject();
+        for (int i = 0; i < 5; i++) {
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    log.info("---1");
+                    waitNotifyObject.allWaitForRunning(1000);
+                    log.info("---2");
+                    waitNotifyObject.removeFromWaitingThreadTable();
+                }
+            });
+            log.info("thread start, then join");
+            t.start();
+            t.join();
+            log.info("thread run end.");
+            // 顺序执行，.join等待thread执行结束
+        }
+        Assert.assertEquals(0, waitNotifyObject.waitingThreadTable.size());
+    }
+
+}
