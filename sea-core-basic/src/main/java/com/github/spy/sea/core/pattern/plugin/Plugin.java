b5ee82ef8aca7a5ed7ecee1778551cf4d14dc9dc
shipengyan <shipengyanvip@163.com>
2021-11-04 14:53:12 +0800

    plugin

---
+++ sea-core-basic/src/main/java/com/github/spy/sea/core/pattern/plugin/Plugin.java
@@ -0,0 +1,50 @@
+package com.github.spy.sea.core.pattern.plugin;
+
+import com.github.spy.sea.core.annotation.Beta;
+
+/**
+ * module name
+ *
+ * @author spy
+ * @version 1.0 2021/11/4
+ * @since 1.0
+ */
+@Beta
+public interface Plugin {
+
+    /**
+     * execute biz
+     *
+     * @param context
+     * @param chain
+     */
+    void execute(PluginContext context, PluginChain chain);
+
+    /**
+     * return plugin order .
+     * This attribute To determine the plugin execution order in the same type plugin.
+     *
+     * @return int order
+     */
+    int getOrder();
+
+    /**
+     * acquire plugin name.
+     *
+     * @return plugin name.
+     */
+    default String named() {
+        return "";
+    }
+
+    /**
+     * plugin is execute.
+     * if return true this plugin can not execute.
+     *
+     * @param context the current server exchange
+     * @return default false.
+     */
+    default boolean skip(PluginContext context) {
+        return false;
+    }
+}
---
+++ sea-core-basic/src/main/java/com/github/spy/sea/core/pattern/plugin/PluginChain.java
@@ -0,0 +1,13 @@
+package com.github.spy.sea.core.pattern.plugin;
+
+/**
+ * module name
+ *
+ * @author spy
+ * @version 1.0 2021/11/4
+ * @since 1.0
+ */
+public interface PluginChain {
+
+    void execute(PluginContext context);
+}
