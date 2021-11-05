6567b3670c3973b665a1e3fe6025bb0e4cebc10b
shipengyan <shipengyanvip@163.com>
2021-11-05 13:26:11 +0800

    ScheduledThreadPoolUtilTest

---
+++ sea-core-basic/src/test/java/com/github/spy/sea/core/thread/ScheduledThreadPoolUtilTest.java
@@ -0,0 +1,38 @@
+package com.github.spy.sea.core.thread;
+
+import com.github.spy.sea.core.BaseCoreTest;
+import com.github.spy.sea.core.thread.util.ScheduledThreadPoolUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * module name
+ *
+ * @author spy
+ * @version 1.0 2021/11/5
+ * @since 1.0
+ */
+@Slf4j
+public class ScheduledThreadPoolUtilTest extends BaseCoreTest {
+
+    @Test
+    public void test17() throws Exception {
+
+        ScheduledThreadPoolExecutor stpe = ScheduledThreadPoolUtil.get("sea-test-sch");
+
+        // 只执行一次
+        stpe.schedule(() -> {
+            log.info("----run");
+        }, 5, TimeUnit.SECONDS);
+
+        // 周期性执行
+        stpe.scheduleAtFixedRate(() -> {
+            log.info("zzzz");
+        }, 5, 5, TimeUnit.SECONDS);
+        sleepSecond(30);
+    }
+
+}
