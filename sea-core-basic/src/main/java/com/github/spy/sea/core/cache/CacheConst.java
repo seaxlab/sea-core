72a6f8e6bd2d68ecb06159cdc915490b24e15667
shipengyan <shipengyanvip@163.com>
2021-12-03 11:10:39 +0800

    common guava cache remove listener

--- sea-core-basic/src/main/java/com/github/spy/sea/core/cache/CacheConst.java
+++ sea-core-basic/src/main/java/com/github/spy/sea/core/cache/CacheConst.java
@@ -1,5 +1,6 @@
 package com.github.spy.sea.core.cache;
 
+import com.google.common.cache.RemovalListener;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -21,4 +22,13 @@ public class CacheConst {
      * 空数组
      */
     public static final String EMPTY_ARRAY = "[]";
+
+    /**
+     * guava common remove listener
+     */
+    public static final RemovalListener COMMON_REMOVE_LISTENER = (RemovalListener<String, Object>) notification -> {
+        if (notification != null) {
+            log.info("remove cache key={}", notification.getKey());
+        }
+    };
 }
