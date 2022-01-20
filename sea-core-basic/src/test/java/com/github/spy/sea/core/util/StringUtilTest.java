db13053adf1f660e2c9153d0539298eab93d2170
shipengyan <shipengyanvip@163.com>
2022-01-20 14:29:47 +0800

    test

--- sea-core-basic/src/test/java/com/github/spy/sea/core/util/StringUtilTest.java
+++ sea-core-basic/src/test/java/com/github/spy/sea/core/util/StringUtilTest.java
@@ -24,48 +24,47 @@ public class StringUtilTest extends BaseCoreTest {
     }
 
     @Test
-    public void run16() throws Exception {
-
+    public void testToCamelCase() throws Exception {
         Assert.assertEquals(StringUtil.toCamelCase("hello_world"), "helloWorld");
+    }
 
-
+    @Test
+    public void testUniqueKey() throws Exception {
         Assert.assertEquals("1:s", StringUtil.uniqueKey(1, "s"));
         Assert.assertEquals("1-s", StringUtil.uniqueKey("-", 1, "s"));
     }
 
     @Test
-    public void joinTest() throws Exception {
+    public void testJoin() throws Exception {
         Assert.assertEquals("1:s", StringUtil.join(":", "1", "s"));
         Assert.assertEquals("1:s", StringUtil.join(":", "1", "", "s"));
         Assert.assertEquals("1:s", StringUtil.join(":", "1", null, "s", null));
-
     }
 
     @Test
-    public void splitTest() throws Exception {
-
+    public void testSplit() throws Exception {
         log.info("{}", StringUtil.splitToIterable("abc;;bac;hi", ';'));
         log.info("{}", StringUtil.splitToList("abc;;bac;hi", ';'));
         log.info("{}", StringUtil.splitToList(null, ';'));
     }
 
     @Test
-    public void run36() throws Exception {
+    public void testDefaultIfBlank() throws Exception {
         Assert.assertEquals("", StringUtil.defaultIfBlank(null, ""));
     }
 
     @Test
-    public void run41() throws Exception {
+    public void testReplaceStart() throws Exception {
         Assert.assertEquals("a0a", StringUtil.replaceStart("00a0a", "0"));
     }
 
     @Test
-    public void removeEmojiTest() throws Exception {
+    public void testRemoveEmoji() throws Exception {
         log.info(StringUtil.removeEmoji("abc,ef123！高兴😄高兴"));
     }
 
     @Test
-    public void run59() throws Exception {
+    public void testContains() throws Exception {
         Assert.assertEquals(true, StringUtil.contains("abcdefg", "abc"));
         Assert.assertEquals(false, StringUtil.contains("abcdefg", "defs"));
         Assert.assertEquals(false, StringUtil.contains(null, "defs"));
@@ -74,17 +73,17 @@ public class StringUtilTest extends BaseCoreTest {
     }
 
     @Test
-    public void run68() throws Exception {
+    public void testContains2() throws Exception {
         log.info("{}", "abc".contains(""));
     }
 
     @Test
-    public void startsWithTest() throws Exception {
+    public void testStartsWith() throws Exception {
         log.info("ret={}", StringUtil.startsWith("qingdao_uat", true, "qingdao"));
     }
 
     @Test
-    public void endsWithTest() throws Exception {
+    public void testEndsWith() throws Exception {
         log.info("ret={}", StringUtil.endsWith("qingdao_uat", true, "uat", "sit", "dev"));
     }
 
@@ -95,7 +94,7 @@ public class StringUtilTest extends BaseCoreTest {
     }
 
     @Test
-    public void test88() throws Exception {
+    public void testAddZeroLeft() throws Exception {
         log.info("{}", System.getProperties());
         log.info("--------");
         log.info("{}", System.getenv());
