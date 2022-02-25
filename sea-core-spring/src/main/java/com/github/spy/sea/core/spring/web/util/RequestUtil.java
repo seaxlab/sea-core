3f5aa655c80715117bdbbf4bb87272eda39bda65
shipengyan <shipengyanvip@163.com>
2022-02-25 09:12:17 +0800

    dynamic register controller

--- sea-core-spring/src/main/java/com/github/spy/sea/core/spring/web/util/RequestUtil.java
+++ sea-core-spring/src/main/java/com/github/spy/sea/core/spring/web/util/RequestUtil.java
@@ -6,13 +6,20 @@ import com.github.spy.sea.core.web.common.WebConst;
 import com.google.common.base.Preconditions;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpCookie;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.util.ClassUtils;
+import org.springframework.util.ReflectionUtils;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Enumeration;
 
@@ -92,9 +99,7 @@ public final class RequestUtil {
      * @param request
      */
     public static void logSimple(ServerHttpRequest request) {
-        log.info("sea request log: [{},{}] id={},queryString={},userAgent={},ip={}",
-                request.getMethodValue(), request.getPath(),
-                request.getId(), request.getQueryParams(), getUserAgent(request), getClientIpAddress(request));
+        log.info("sea request log: [{},{}] id={},queryString={},userAgent={},ip={}", request.getMethodValue(), request.getPath(), request.getId(), request.getQueryParams(), getUserAgent(request), getClientIpAddress(request));
     }
 
     /**
@@ -125,9 +130,7 @@ public final class RequestUtil {
         sb.append("ip : ").append(request.getRemoteAddr()).append("\n");
         sb.append("args : ").append(Arrays.toString(joinPoint.getArgs())).append("\n");
 
-        sb.append("Controller : ").append(joinPoint.getTarget().getClass().getName()).append(".(")
-          .append(joinPoint.getTarget().getClass().getSimpleName()).append(".java:1) ").
-          append(joinPoint.getSignature().getName()).append("\n");
+        sb.append("Controller : ").append(joinPoint.getTarget().getClass().getName()).append(".(").append(joinPoint.getTarget().getClass().getSimpleName()).append(".java:1) ").append(joinPoint.getSignature().getName()).append("\n");
 
         String uri = request.getRequestURI();
         if (uri != null) {
@@ -145,8 +148,7 @@ public final class RequestUtil {
                 } else {
                     sb.append(name).append("[]={");
                     for (int i = 0; i < values.length; i++) {
-                        if (i > 0)
-                            sb.append(",");
+                        if (i > 0) sb.append(",");
                         sb.append(values[i]);
                     }
                     sb.append("}");
@@ -184,6 +186,61 @@ public final class RequestUtil {
         }
 
         return "";
+    }
+
+    /**
+     * 动态注册Controller
+     *
+     * @param ctx             spring上下文
+     * @param requestPath     请求路径
+     * @param requestMethod   请求方法
+     * @param controllerClass 控制器类
+     * @param methodName      请求方法
+     * @param parameterTypes  请求方法参数
+     */
+    public static void registerMapping(ApplicationContext ctx, String requestPath, RequestMethod requestMethod, Class<?> controllerClass, String methodName, Class<?>... parameterTypes) {
+        if (requestMethod == null) {
+            requestMethod = RequestMethod.GET;
+        }
+        RequestMappingInfo mappingInfo = RequestMappingInfo.paths(requestPath).methods(requestMethod).build();
+
+        try {
+            Method method = controllerClass.getClass().getMethod(methodName, parameterTypes);
+
+            RequestMappingHandlerMapping requestMappingHandlerMapping = (RequestMappingHandlerMapping) ctx.getBean("requestMappingHandlerMapping");
+            requestMappingHandlerMapping.registerMapping(mappingInfo, controllerClass, method);
+        } catch (Exception e) {
+            log.error("fail to register dynamic controller", e);
+        }
+    }
+
+    /**
+     * 取消之前注册的Controller
+     *
+     * @param ctx            spring上下文
+     * @param controllerName controller名称
+     */
+    public static void unregisterMapping(ApplicationContext ctx, String controllerName) {
+        RequestMappingHandlerMapping requestMappingHandlerMapping = (RequestMappingHandlerMapping) ctx.getBean("requestMappingHandlerMapping");
+        Object controller = ctx.getBean(controllerName);
+        if (controller == null) {
+            log.warn("controller[{}] is not in container", controllerName);
+            return;
+        }
+        Class<?> targetClass = controller.getClass();
+        ReflectionUtils.doWithMethods(targetClass, method -> {
+            Method specificMethod = ClassUtils.getMostSpecificMethod(method, targetClass);
+            try {
+                Method createMappingMethod = RequestMappingHandlerMapping.class.getDeclaredMethod("getMappingForMethod", Method.class, Class.class);
+                createMappingMethod.setAccessible(true);
+                RequestMappingInfo requestMappingInfo = (RequestMappingInfo) createMappingMethod.invoke(requestMappingHandlerMapping, specificMethod, targetClass);
+                if (requestMappingInfo != null) {
+                    requestMappingHandlerMapping.unregisterMapping(requestMappingInfo);
+                }
+            } catch (Exception e) {
+                log.warn("fail to reflect unregister controller method", e);
+            }
+        }, ReflectionUtils.USER_DECLARED_METHODS);
     }
 
 }
