8cf7c89e2abbf13a0d5de5f78a5c8a9a2c7dbc7b
shipengyan <shipengyanvip@163.com>
2022-03-17 10:09:37 +0800

    Yes/No boolean type handler

---
+++ sea-core-dal/src/main/java/com/github/spy/sea/core/dal/mybatis/common/handler/YesNoBooleanTypeHandler.java
@@ -0,0 +1,54 @@
+package com.github.spy.sea.core.dal.mybatis.common.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * <p>优先使用BooleanTypeHandler，可以处理0/1类型的</p>
+ * <p>这里扩展N/Y值</p>
+ *
+ * @author spy
+ * @version 1.0 2022/3/17
+ * @since 1.0
+ */
+@Slf4j
+public class YesNoBooleanTypeHandler extends BaseTypeHandler<Boolean> {
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, Boolean parameter, JdbcType jdbcType)
+            throws SQLException {
+        ps.setString(i, convert(parameter));
+    }
+
+    @Override
+    public Boolean getNullableResult(ResultSet rs, String columnName)
+            throws SQLException {
+        return convert(rs.getString(columnName));
+    }
+
+    @Override
+    public Boolean getNullableResult(ResultSet rs, int columnIndex)
+            throws SQLException {
+        return convert(rs.getString(columnIndex));
+    }
+
+    @Override
+    public Boolean getNullableResult(CallableStatement cs, int columnIndex)
+            throws SQLException {
+        return convert(cs.getString(columnIndex));
+    }
+
+    private String convert(Boolean b) {
+        return b ? "Y" : "N";
+    }
+
+    private Boolean convert(String s) {
+        return s.equals("Y");
+    }
+}
