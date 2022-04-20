f9184615f63be1b8aaa4668fd3545252c581087b
shipengyan <shipengyanvip@163.com>
2022-04-20 15:15:55 +0800

    拆分

--- sea-core-dubbo-legacy/src/test/java/com/github/spy/sea/core/dubbo/legacy/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * module name
- *
- * @author spy
- * @version 1.0 2020/4/22
- * @since 1.0
- */
-package com.github.spy.sea.core.dubbo.legacy;
--- sea-core-script/src/main/java/com/github/spy/sea/core/script/groovy/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * groovy脚本引擎
- *
- * @author spy
- * @version 1.0 2019-07-22
- * @since 1.0
- */
-package com.github.spy.sea.core.script.groovy;
