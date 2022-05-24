eb16cd14344c304a4f0ecdea778ed37d4a9dcb28
shipengyan <shipengyanvip@163.com>
2022-05-24 14:56:26 +0800

    DES util

---
+++ sea-core-basic/src/main/java/com/github/spy/sea/core/security/util/DESUtil.java
@@ -0,0 +1,145 @@
+package com.github.spy.sea.core.security.util;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import javax.crypto.spec.IvParameterSpec;
+
+/**
+ * DES util
+ *
+ * @author spy
+ * @version 1.0 2022/5/24
+ * @since 1.0
+ */
+@Slf4j
+public class DESUtil {
+
+    //DES_CBC_Encrypt
+
+    /**
+     * encrypt
+     *
+     * @param secretKey 重点：必须是8位
+     * @param str
+     * @return
+     */
+    public static String encrypt(String secretKey, String str) {
+        try {
+            byte[] keyBytes = secretKey.getBytes();
+            byte[] content = str.getBytes();
+            DESKeySpec keySpec = new DESKeySpec(keyBytes);
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+            SecretKey key = keyFactory.generateSecret(keySpec);
+
+            Cipher cipher = Cipher.getInstance("DES/CBC/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, key, new IvParameterSpec(keySpec.getKey()));
+            byte[] result = cipher.doFinal(content);
+            return byteToHexString(result);
+        } catch (Exception e) {
+            log.error("fail to encrypt by DES", e);
+        }
+        return null;
+    }
+
+    /**
+     * decrypt
+     *
+     * @param secretKey
+     * @param str
+     * @return
+     */
+    public static String decrypt(String secretKey, String str) {
+        try {
+            byte[] keyBytes = secretKey.getBytes();
+            byte[] content = hexToByteArray(str);
+            DESKeySpec keySpec = new DESKeySpec(keyBytes);
+            SecretKeyFactory keyFactory = SecretKeyFactory.getInstance("DES");
+            SecretKey key = keyFactory.generateSecret(keySpec);
+
+            Cipher cipher = Cipher.getInstance("DES/CBC/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(keyBytes));
+            byte[] result = cipher.doFinal(content);
+            return new String(result);
+        } catch (Exception e) {
+            log.error("fail to decrypt by DES", e);
+        }
+        return null;
+    }
+
+    private static String byteToHexString(byte[] bytes) {
+        StringBuilder sb = new StringBuilder(bytes.length);
+        String sTemp;
+        for (byte aByte : bytes) {
+            sTemp = Integer.toHexString(0xFF & aByte);
+            if (sTemp.length() < 2)
+                sb.append(0);
+            sb.append(sTemp.toUpperCase());
+        }
+        return sb.toString();
+    }
+
+    private static byte[] hexToByteArray(String inHex) {
+        int hexLen = inHex.length();
+        byte[] result;
+        if (hexLen % 2 == 1) {
+            hexLen++;
+            result = new byte[(hexLen / 2)];
+            inHex = "0" + inHex;
+        } else {
+            result = new byte[(hexLen / 2)];
+        }
+        int j = 0;
+        for (int i = 0; i < hexLen; i += 2) {
+            result[j] = (byte) Integer.parseInt(inHex.substring(i, i + 2), 16);
+            j++;
+        }
+        return result;
+    }
+
+    //public static String encrypt(String text) {
+    //    try {
+    //        KeyGenerator keygenerator = KeyGenerator.getInstance("DES");
+    //        SecretKey myDesKey = keygenerator.generateKey();
+    //
+    //        // Create the cipher
+    //        Cipher desCipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
+    //
+    //        // Initialize the cipher for encryption
+    //        desCipher.init(Cipher.ENCRYPT_MODE, myDesKey);
+    //
+    //        //sensitive information
+    //        return Base64.encodeBase64String(text.getBytes());
+    //    } catch (Exception e) {
+    //        log.warn("fail to encrypt by DES", e);
+    //    }
+    //    return "";
+    //}
+    //
+    //
+    //public static String decrypt(String text) {
+    //
+    //    try {
+    //        byte[] bytes = Base64.decodeBase64(text.getBytes());
+    //        KeyGenerator keygenerator = KeyGenerator.getInstance("DES");
+    //        SecretKey myDesKey = keygenerator.generateKey();
+    //
+    //        Cipher desCipher = Cipher.getInstance("DES/ECB/PKCS5Padding");
+    //
+    //        // Initialize the same cipher for decryption
+    //        desCipher.init(Cipher.DECRYPT_MODE, myDesKey);
+    //
+    //        // Decrypt the text
+    //        byte[] textDecrypted = desCipher.doFinal(bytes);
+    //
+    //        return new String(textDecrypted);
+    //    } catch (Exception e) {
+    //        log.error("fail to decrypt by DES", e);
+    //    }
+    //    return "";
+    //}
+
+}
