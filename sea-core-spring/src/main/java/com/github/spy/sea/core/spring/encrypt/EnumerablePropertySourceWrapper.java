02985aa23a28f00c24ba6e5c28ee64ab13c6b5bc
shipengyan <shipengyanvip@163.com>
2022-05-24 14:57:16 +0800

    encrypt properties/yml

---
+++ sea-core-spring/src/main/java/com/github/spy/sea/core/spring/encrypt/EnumerablePropertySourceWrapper.java
@@ -0,0 +1,32 @@
+package com.github.spy.sea.core.spring.encrypt;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.EnumerablePropertySource;
+
+/**
+ * module name
+ *
+ * @author spy
+ * @version 1.0 2022/5/24
+ * @since 1.0
+ */
+@Slf4j
+public class EnumerablePropertySourceWrapper<T> extends EnumerablePropertySource<T> {
+
+    private final PropertySourceWrapper<T> delegate;
+
+    public EnumerablePropertySourceWrapper(EnumerablePropertySource<T> delegate) {
+        super(delegate.getName(), delegate.getSource());
+        this.delegate = new PropertySourceWrapper(delegate);
+    }
+
+    @Override
+    public Object getProperty(String name) {
+        return delegate.getProperty(name);
+    }
+
+    @Override
+    public String[] getPropertyNames() {
+        return ((EnumerablePropertySource) delegate.getDelegate()).getPropertyNames();
+    }
+}
