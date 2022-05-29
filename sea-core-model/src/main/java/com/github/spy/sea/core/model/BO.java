8ea1ee62e4392001ea141bd484be500c5e44ecc7
shipengyan <shipengyanvip@163.com>
2022-05-29 10:23:39 +0800

    add doc

--- sea-core-model/src/main/java/com/github/spy/sea/core/model/BO.java
+++ sea-core-model/src/main/java/com/github/spy/sea/core/model/BO.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 /**
  * BO
  *
+ * <p>禁止使用BO</p>
+ *
  * @author spy
  * @version 1.0 11/17/20
  * @since 1.0
--- sea-core-model/src/main/java/com/github/spy/sea/core/model/POJO.java
+++ sea-core-model/src/main/java/com/github/spy/sea/core/model/POJO.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 /**
  * POJO
+ * <p>禁止使用POJO，POJO是一个统称</p>
  *
  * @author spy
  * @version 1.0 2020/9/11
