3c003317335b6fc7134990cac990621fc048073e
shipengyan <shipengyanvip@163.com>
2022-06-06 11:08:01 +0800

    encryptor in spring property/yml

---
+++ sea-core-example/src/main/java/com/github/spy/sea/core/example/config/BeanConfig.java
@@ -0,0 +1,23 @@
+package com.github.spy.sea.core.example.config;
+
+import com.github.spy.sea.core.spring.component.encrypt.EncryptPropertiesBeanFactoryPostProcessor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+/**
+ * module name
+ *
+ * @author spy
+ * @version 1.0 2022/5/24
+ * @since 1.0
+ */
+@Slf4j
+@Configuration
+public class BeanConfig {
+
+    //@Bean
+    public EncryptPropertiesBeanFactoryPostProcessor encryptPropertiesBeanFactoryPostProcessor(ConfigurableEnvironment environment) {
+        return new EncryptPropertiesBeanFactoryPostProcessor(environment);
+    }
+}
