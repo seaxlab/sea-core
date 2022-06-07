5180a04763920ac68e897195fded20ac3f16c368
shipengyan <shipengyanvip@163.com>
2022-06-07 15:34:22 +0800

    refine merged annotation

--- sea-core-basic/src/main/java/com/github/spy/sea/core/component/sensitive/fastjson/core/SensitiveSerializeValueFilter.java
+++ sea-core-basic/src/main/java/com/github/spy/sea/core/component/sensitive/fastjson/core/SensitiveSerializeValueFilter.java
@@ -3,10 +3,8 @@ package com.github.spy.sea.core.component.sensitive.fastjson.core;
 import com.alibaba.fastjson.serializer.ValueFilter;
 import com.github.spy.sea.core.component.sensitive.fastjson.annotation.SensitiveInfo;
 import com.github.spy.sea.core.component.sensitive.strategy.IStrategy;
+import com.github.spy.sea.core.lang.annotation.MergedAnnotation;
 import lombok.extern.slf4j.Slf4j;
-
-import javax.annotation.*;
-import java.lang.annotation.*;
 
 /**
  * module name
@@ -26,40 +24,40 @@ public class SensitiveSerializeValueFilter implements ValueFilter {
     public Object process(Object object, String name, Object value) {
 
         try {
-            //TODO 这里可以增加缓存
-            SensitiveInfo sensitiveInfoAnno = object.getClass().getDeclaredField(name).getAnnotation(SensitiveInfo.class);
+            MergedAnnotation mergedAnnotation = MergedAnnotation.from(object.getClass().getDeclaredField(name));
+            final SensitiveInfo sensitiveInfoAnno = mergedAnnotation.getAnnotation(SensitiveInfo.class);
 
-            if (sensitiveInfoAnno == null) {
-                // 获取字段上所有的注解
-                Annotation[] annos = object.getClass().getDeclaredField(name).getAnnotations();
-                if (annos == null || annos.length == 0) {
-                    return value;
-                }
-
-                for (Annotation anno : annos) {
-                    if (anno.annotationType() == Deprecated.class &&
-                            anno.annotationType() == SuppressWarnings.class &&
-                            anno.annotationType() == Override.class &&
-                            anno.annotationType() == PostConstruct.class &&
-                            anno.annotationType() == PreDestroy.class &&
-                            anno.annotationType() == Resource.class &&
-                            anno.annotationType() == Resources.class &&
-                            anno.annotationType() == Generated.class &&
-                            anno.annotationType() == Target.class &&
-                            anno.annotationType() == Retention.class &&
-                            anno.annotationType() == Documented.class &&
-                            anno.annotationType() == Inherited.class
-                    ) {
-
-                    } else {
-                        // JDK 原生注解不具备combo-annotation特性，因此这里再获取一次
-                        sensitiveInfoAnno = anno.annotationType().getAnnotation(SensitiveInfo.class);
-                        if (sensitiveInfoAnno != null) {
-                            break;
-                        }
-                    }
-                }
-            }
+            //if (sensitiveInfoAnno == null) {
+            //    // 获取字段上所有的注解
+            //    Annotation[] annos = object.getClass().getDeclaredField(name).getAnnotations();
+            //    if (annos == null || annos.length == 0) {
+            //        return value;
+            //    }
+            //
+            //    for (Annotation anno : annos) {
+            //        if (anno.annotationType() == Deprecated.class &&
+            //                anno.annotationType() == SuppressWarnings.class &&
+            //                anno.annotationType() == Override.class &&
+            //                anno.annotationType() == PostConstruct.class &&
+            //                anno.annotationType() == PreDestroy.class &&
+            //                anno.annotationType() == Resource.class &&
+            //                anno.annotationType() == Resources.class &&
+            //                anno.annotationType() == Generated.class &&
+            //                anno.annotationType() == Target.class &&
+            //                anno.annotationType() == Retention.class &&
+            //                anno.annotationType() == Documented.class &&
+            //                anno.annotationType() == Inherited.class
+            //        ) {
+            //
+            //        } else {
+            //            // JDK 原生注解不具备combo-annotation特性，因此这里再获取一次
+            //            sensitiveInfoAnno = anno.annotationType().getAnnotation(SensitiveInfo.class);
+            //            if (sensitiveInfoAnno != null) {
+            //                break;
+            //            }
+            //        }
+            //    }
+            //}
 
             if (sensitiveInfoAnno == null) {
                 return value;
