2a98efa15fa6a8455c20bb0b0608e6345d80c112
shipengyan <taotao365s@163.com>
2022-08-11 16:55:28 +0800

    type handler

---
+++ sea-core-dal/src/main/java/com/github/seaxlab/core/dal/mybatis/plus/handler/LongAndDate2TypeHandler.java
@@ -0,0 +1,63 @@
+package com.github.seaxlab.core.dal.mybatis.plus.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.MappedJdbcTypes;
+import org.apache.ibatis.type.MappedTypes;
+
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+
+/**
+ * Long and Date type handler
+ *
+ * @author spy
+ * @version 1.0 2022/8/8
+ * @since 1.0
+ */
+@Slf4j
+@MappedTypes({Date.class})
+@MappedJdbcTypes(JdbcType.BIGINT)
+public class LongAndDate2TypeHandler extends BaseTypeHandler<Date> {
+
+    @Override
+    public void setNonNullParameter(PreparedStatement ps, int i, Date date, JdbcType jdbcType) throws SQLException {
+        ps.setLong(i, date == null ? new Date().getTime() : date.getTime());
+    }
+
+    @Override
+    public Date getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        Long value = rs.getLong(columnName);
+        if (value == null || value <= 0) {
+            // value=-1 or 0, no meaning.
+            return null;
+        }
+
+        return new Date(value);
+    }
+
+    @Override
+    public Date getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        Long value = rs.getLong(columnIndex);
+        if (value == null || value <= 0) {
+            return null;
+        }
+
+        return new Date(value);
+    }
+
+    @Override
+    public Date getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
+        Long value = cs.getLong(columnIndex);
+        if (value == null || value <= 0) {
+            return null;
+        }
+
+        return new Date(value);
+    }
+
+}
