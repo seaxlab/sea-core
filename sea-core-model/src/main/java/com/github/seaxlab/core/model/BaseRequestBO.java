6a9c089cbed24ba1e4e91f1db2ed7a9147762a35
shipengyan <taotao365s@163.com>
2022-08-26 15:44:58 +0800

    base request/response BO

---
+++ sea-core-model/src/main/java/com/github/seaxlab/core/model/BaseRequestBO.java
@@ -0,0 +1,11 @@
+package com.github.seaxlab.core.model;
+
+/**
+ * base request bo
+ *
+ * @author spy
+ * @version 1.0 2022/8/26
+ * @since 1.0
+ */
+public class BaseRequestBO extends BO {
+}
---
+++ sea-core-model/src/main/java/com/github/seaxlab/core/model/BaseResponseBO.java
@@ -0,0 +1,11 @@
+package com.github.seaxlab.core.model;
+
+/**
+ * base response bo
+ *
+ * @author spy
+ * @version 1.0 2022/8/26
+ * @since 1.0
+ */
+public class BaseResponseBO extends BO {
+}
