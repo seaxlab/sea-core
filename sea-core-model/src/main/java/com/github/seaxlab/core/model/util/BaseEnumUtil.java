59ae87c17cb302b66a78ec3bae94a3f02a9c589c
shipengyan <taotao365s@163.com>
2022-09-06 13:39:53 +0800

    base enum util

--- sea-core-model/src/main/java/com/github/seaxlab/core/model/util/BaseEnumUtil.java
+++ sea-core-model/src/main/java/com/github/seaxlab/core/model/util/BaseEnumUtil.java
@@ -3,6 +3,9 @@ package com.github.seaxlab.core.model.util;
 import com.github.seaxlab.core.enums.IBaseEnum;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 /**
  * Base Enum util
  *
@@ -23,6 +26,7 @@ public final class BaseEnumUtil {
      */
     public static <T> T getCode(IBaseEnum<T> typeEnum) {
         if (typeEnum == null) {
+            log.warn("type enum is null");
             return null;
         }
         return typeEnum.getCode();
@@ -36,9 +40,70 @@ public final class BaseEnumUtil {
      */
     public static <T> String getDesc(IBaseEnum<T> typeEnum) {
         if (typeEnum == null) {
+            log.warn("type enum is null");
             return null;
         }
         return typeEnum.getDesc();
+    }
+
+    /**
+     * to enum
+     *
+     * @param code
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T extends IBaseEnum<Integer>> Optional<T> toEnum(int code, Class<T> clazz) {
+        return Arrays.stream(clazz.getEnumConstants()) //
+                .filter(baseEnum -> baseEnum.getCode().intValue() == code) //
+                .findAny();
+    }
+
+    /**
+     * to enum
+     *
+     * @param code
+     * @param clazz
+     * @param defaultValue
+     * @param <T>
+     * @return
+     */
+    public static <T extends IBaseEnum<Integer>> T toEnum(int code, Class<T> clazz, T defaultValue) {
+        return Arrays.stream(clazz.getEnumConstants()) //
+                .filter(baseEnum -> baseEnum.getCode().intValue() == code) //
+                .findAny() //
+                .orElse(defaultValue);
+    }
+
+    /**
+     * to enum
+     *
+     * @param code
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T extends IBaseEnum<String>> Optional<T> toEnum(String code, Class<T> clazz) {
+        return Arrays.stream(clazz.getEnumConstants()) //
+                .filter(baseEnum -> baseEnum.getCode().equalsIgnoreCase(code)) //
+                .findAny();
+    }
+
+    /**
+     * to enum
+     *
+     * @param code
+     * @param clazz
+     * @param defaultValue
+     * @param <T>
+     * @return
+     */
+    public static <T extends IBaseEnum<String>> T toEnum(String code, Class<T> clazz, T defaultValue) {
+        return Arrays.stream(clazz.getEnumConstants()) //
+                .filter(baseEnum -> baseEnum.getCode().equalsIgnoreCase(code)) //
+                .findAny() //
+                .orElse(defaultValue);
     }
 
 }
