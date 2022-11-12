ae150cf3332235fba17c83d3a54834eeace393dc
shipengyan <taotao365s@163.com>
2022-11-12 14:19:53 +0800

    model cost

--- sea-core-model/src/main/java/com/github/seaxlab/core/model/common/ModelConst.java
+++ sea-core-model/src/main/java/com/github/seaxlab/core/model/common/ModelConst.java
@@ -12,75 +12,77 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public final class ModelConst {
 
-    // field
-    // ID
-    public static final String ID = "id";
-    // 关联id
-    public static final String REF_ID = "refId";
-    // 关联code
-    public static final String REF_CODE = "refCode";
-    // 关联key
-    public static final String REF_KEY = "refKey";
-    // 关联单号
-    public static final String REF_NO = "refNo";
+  // field
+  // ID
+  public static final String ID = "id";
+  // 关联id
+  public static final String REF_ID = "refId";
+  // 关联code
+  public static final String REF_CODE = "refCode";
+  // 关联key
+  public static final String REF_KEY = "refKey";
+  // 关联单号
+  public static final String REF_NO = "refNo";
 
-    public static final String REF_BIZ_KEY = "refBizKey";
+  public static final String REF_BIZ_KEY = "refBizKey";
 
-    //外部订单号(商户订单号)
-    public static final String OUT_TRADE_NO = "outTradeNo";
-    // 外部请求单号
-    public static final String OUT_REQUEST_NO = "outRequestNo";
-    // 单号
-    public static final String ORDER_NO = "orderNo";
-    // 子单号
-    public static final String SUB_ORDER_NO = "subOrderNo";
-    // 流水号
-    public static final String SERIAL_NUMBER = "serialNumber";
-    // 交易号
-    public static final String TRANS_NO = "transNo";
-
-
-    // 申请单-单号
-    public static final String APPLY_NO = "applyNo";
-    // 申请单-状态
-    public static final String APPLY_STATUS = "applyStatus";
-
-    public static final String INVOICE_NO = "invoiceNo";
-
-    // 账户相关, customer(租户)-> user(用户) -> account(账户)
-    public static final String CUSTOMER_NO = "customerNo";
-    public static final String USER_NO = "userNo";
-    public static final String ACCOUNT_NO = "accountNo";
-
-    public static final String CUSTOMER_ID = "customerId";
-    public static final String USER_ID = "userId";
-    public static final String ACCOUNT_ID = "accountId";
+  //外部订单号(商户订单号)
+  public static final String OUT_TRADE_NO = "outTradeNo";
+  // 外部请求单号
+  public static final String OUT_REQUEST_NO = "outRequestNo";
+  // 单号
+  public static final String ORDER_NO = "orderNo";
+  // 子单号
+  public static final String SUB_ORDER_NO = "subOrderNo";
+  // 流水号
+  public static final String SERIAL_NUMBER = "serialNumber";
+  // 交易号
+  public static final String TRANS_NO = "transNo";
+  // 行号
+  public static final String ROW_NO = "rowNo";
 
 
-    public static final String CODE = "code";
-    public static final String INNER_CODE = "innerCode";
-    public static final String STANDARD_CODE = "standardCode";
-    public static final String UNIFIED_CODE = "unifiedCode";
-    public static final String GROUP_CODE = "groupCode";
-    public static final String PARENT_CODE = "parentCode";
+  // 申请单-单号
+  public static final String APPLY_NO = "applyNo";
+  // 申请单-状态
+  public static final String APPLY_STATUS = "applyStatus";
 
-    public static final String NAME = "name";
-    public static final String TYPE = "type";
-    public static final String CATEGORY = "category";
-    public static final String BIZ_TYPE = "bizType";
-    public static final String BIZ_TYPE_NAME = "bizTypeName";
-    public static final String BIZ_SUB_TYPE = "bizSubType";
-    public static final String BIZ_SUB_TYPE_NAME = "bizSubTypeName";
+  public static final String INVOICE_NO = "invoiceNo";
 
-    public static final String BIZ_TYPE_CODE = "bizTypeCode";
-    public static final String BIZ_SUB_TYPE_CODE = "bizSubTypeCode";
+  // 账户相关, customer(租户)-> user(用户) -> account(账户)
+  public static final String CUSTOMER_NO = "customerNo";
+  public static final String USER_NO = "userNo";
+  public static final String ACCOUNT_NO = "accountNo";
 
-    // task
-    public static final String TASK_CODE = "taskCode";
-    public static final String TASK_NAME = "taskName";
+  public static final String CUSTOMER_ID = "customerId";
+  public static final String USER_ID = "userId";
+  public static final String ACCOUNT_ID = "accountId";
 
-    // 业务键
-    public static final String BIZ_KEY = "bizKey";
+
+  public static final String CODE = "code";
+  public static final String INNER_CODE = "innerCode";
+  public static final String STANDARD_CODE = "standardCode";
+  public static final String UNIFIED_CODE = "unifiedCode";
+  public static final String GROUP_CODE = "groupCode";
+  public static final String PARENT_CODE = "parentCode";
+
+  public static final String NAME = "name";
+  public static final String TYPE = "type";
+  public static final String CATEGORY = "category";
+  public static final String BIZ_TYPE = "bizType";
+  public static final String BIZ_TYPE_NAME = "bizTypeName";
+  public static final String BIZ_SUB_TYPE = "bizSubType";
+  public static final String BIZ_SUB_TYPE_NAME = "bizSubTypeName";
+
+  public static final String BIZ_TYPE_CODE = "bizTypeCode";
+  public static final String BIZ_SUB_TYPE_CODE = "bizSubTypeCode";
+
+  // task
+  public static final String TASK_CODE = "taskCode";
+  public static final String TASK_NAME = "taskName";
+
+  // 业务键
+  public static final String BIZ_KEY = "bizKey";
     // 业务单号
     public static final String BIZ_NO = "bizNo";
 
@@ -183,43 +185,44 @@ public final class ModelConst {
     public static final String SOURCE = "source";
     /* 用户操作时系统渠道 */
     public static final String CHANNEL = "channel";
-    /*场景*/
-    public static final String SCENE = "scene";
-    public static final String SCENE_NAME = "sceneName";
-    public static final String ACTION = "action";
-    public static final String ACTION_TYPE = "actionType";
-    public static final String ACTION_NAME = "actionName";
+  /*场景*/
+  public static final String SCENE = "scene";
+  public static final String SCENE_NAME = "sceneName";
+  public static final String ACTION = "action";
+  public static final String ACTION_TYPE = "actionType";
+  public static final String ACTION_NAME = "actionName";
 
-    public static final String STRATEGY = "strategy";
+  public static final String STRATEGY = "strategy";
 
-    public static final String URL = "url";
-    public static final String IP = "ip";
-    public static final String USER_AGENT = "userAgent";
-    // 设备号、设备标识
-    public static final String DEVICE_NO = "deviceNo";
-    public static final String REQUEST = "request";
-    public static final String RESPONSE = "response";
-    public static final String PARAMETER = "parameter";
-    public static final String BODY = "body";
-    public static final String PAYLOAD = "payload";
-    public static final String RESULT = "result";
+  public static final String URL = "url";
+  public static final String IP = "ip";
+  public static final String USER_AGENT = "userAgent";
+  // 设备号、设备标识
+  public static final String DEVICE_NO = "deviceNo";
+  public static final String REQUEST = "request";
+  public static final String RESPONSE = "response";
+  public static final String PARAMETER = "parameter";
+  public static final String BODY = "body";
+  public static final String PAYLOAD = "payload";
+  public static final String RESULT = "result";
+  public static final String SUCCESS = "success";
 
-    public static final String COUNT = "count";
-    public static final String SIZE = "size";
+  public static final String COUNT = "count";
+  public static final String SIZE = "size";
 
-    public static final String TOKEN = "token";
-    public static final String ACCESS_TOKEN = "accessToken";
-    public static final String REFRESH_TOKEN = "refreshToken";
+  public static final String TOKEN = "token";
+  public static final String ACCESS_TOKEN = "accessToken";
+  public static final String REFRESH_TOKEN = "refreshToken";
 
-    public static final String MOBILE = "mobile";
-    public static final String PHONE = "phone";
+  public static final String MOBILE = "mobile";
+  public static final String PHONE = "phone";
 
-    // value
-    public static final String REMARK_EMPTY = "-";
-    public static final String ID_EMPTY = "0";
-    public static final Integer ID_INT_EMPTY = 0;
-    public static final Long ID_LONG_EMPTY = 0L;
+  // value
+  public static final String REMARK_EMPTY = "-";
+  public static final String ID_EMPTY = "0";
+  public static final Integer ID_INT_EMPTY = 0;
+  public static final Long ID_LONG_EMPTY = 0L;
 
-    private ModelConst() {
-    }
+  private ModelConst() {
+  }
 }
