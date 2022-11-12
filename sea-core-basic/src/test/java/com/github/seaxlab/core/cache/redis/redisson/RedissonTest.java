aa2c5fb8b720699699d6824c66ad4e79c33106ee
shipengyan <taotao365s@163.com>
2022-11-12 14:21:27 +0800

    redisson test

--- sea-core-basic/src/test/java/com/github/seaxlab/core/cache/redis/redisson/RedissonTest.java
+++ sea-core-basic/src/test/java/com/github/seaxlab/core/cache/redis/redisson/RedissonTest.java
@@ -1,17 +1,19 @@
 package com.github.seaxlab.core.cache.redis.redisson;
 
 import com.github.seaxlab.core.BaseCoreTest;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReadWriteLock;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Before;
 import org.junit.Test;
 import org.redisson.Redisson;
 import org.redisson.RedissonMultiLock;
+import org.redisson.api.RBuckets;
 import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.locks.ReadWriteLock;
 
 /**
  * module name
@@ -22,139 +24,157 @@ import java.util.concurrent.locks.ReadWriteLock;
  */
 @Slf4j
 public class RedissonTest extends BaseCoreTest {
-    RedissonClient client;
 
-    @Before
-    public void before() throws Exception {
+  RedissonClient client;
 
-        Config config = new Config();
-        config.useSingleServer()
-              .setAddress("redis://redis:6379");
+  @Before
+  public void before() throws Exception {
 
-        client = Redisson.create(config);
+    Config config = new Config();
+    config.useSingleServer().setAddress("redis://redis:6379");
+
+    client = Redisson.create(config);
+  }
+
+  @Test
+  public void testReadWriteLock() throws Exception {
+    runInMultiThread(() -> {
+      ReadWriteLock innerLock = client.getReadWriteLock("lock1");
+      log.info("inner lock begin ");
+      innerLock.writeLock().lock();
+      log.info("inner lock biz ");
+      sleepSecond(10);
+      innerLock.writeLock().unlock();
+      log.info("inner lock end ");
+
+    });
+
+  }
+
+  @Test
+  public void testTry() throws Exception {
+    RLock lock = client.getLock("lock1");
+    // key会自动续期（leaseTime=-1会自动续期）
+    lock.tryLock();
+    sleepSecond(50);
+    lock.unlock();
+  }
+
+  @Test
+  public void testLock() throws Exception {
+    RLock lock = client.getLock("lock1");
+    boolean flag = lock.tryLock(0, TimeUnit.SECONDS);
+
+    if (!flag) {
+      log.warn("fail to get all lock");
+      return;
     }
 
-    @Test
-    public void testReadWriteLock() throws Exception {
-        runInMultiThread(() -> {
-            ReadWriteLock innerLock = client.getReadWriteLock("lock1");
-            log.info("inner lock begin ");
-            innerLock.writeLock().lock();
-            log.info("inner lock biz ");
-            sleepSecond(10);
-            innerLock.writeLock().unlock();
-            log.info("inner lock end ");
+    try {
+      log.info("do biz");
+      sleepSecond(3);
+    } finally {
+      lock.unlock();
+    }
+    log.info("biz end.");
+  }
 
-        });
+  @Test
+  public void testRelease() throws Exception {
+    String lockKey = "lock1";
+    RLock lock = client.getLock(lockKey);
+    boolean flag = lock.tryLock();
 
+    if (!flag) {
+      log.warn("fail to get all lock");
+      return;
     }
 
-    @Test
-    public void testTry() throws Exception {
-        RLock lock = client.getLock("lock1");
-        // key会自动续期（leaseTime=-1会自动续期）
-        lock.tryLock();
-        sleepSecond(50);
+    RLock lock2 = client.getLock(lockKey);
+    boolean flag2 = lock2.tryLock();
+
+    try {
+      log.info("do biz");
+      sleepSecond(3);
+
+      lock2.unlock();
+    } finally {
+      lock.unlock();
+    }
+    log.info("biz end.");
+  }
+
+  @Test
+  public void testInMultiThread() throws Exception {
+
+    runInMultiThread(() -> {
+      RLock lock = client.getLock("lock1");
+      boolean flag = false;
+      try {
+        flag = lock.tryLock(0, TimeUnit.SECONDS);
+      } catch (InterruptedException e) {
+        log.error("fail to interrupted exception", e);
+      }
+
+      if (!flag) {
+        log.warn("fail to get all lock");
+        return;
+      }
+
+      try {
+        log.info("do biz");
+        sleepSecond(3);
+      } finally {
         lock.unlock();
+      }
+      log.info("biz end.");
+    });
+    sleepSecond(10);
+  }
+
+
+  @Test
+  public void testMultiLock() throws Exception {
+    RLock lock1 = client.getLock("lock1");
+    RLock lock2 = client.getLock("lock2");
+    RLock lock3 = client.getLock("lock3");
+
+    RedissonMultiLock lock = new RedissonMultiLock(lock1, lock2, lock3);
+
+    boolean flag = lock.tryLock(0, TimeUnit.SECONDS);
+
+    if (!flag) {
+      log.warn("fail to get all lock");
+      return;
     }
 
-    @Test
-    public void testLock() throws Exception {
-        RLock lock = client.getLock("lock1");
-        boolean flag = lock.tryLock(0, TimeUnit.SECONDS);
-
-        if (!flag) {
-            log.warn("fail to get all lock");
-            return;
-        }
-
-        try {
-            log.info("do biz");
-            sleepSecond(3);
-        } finally {
-            lock.unlock();
-        }
-        log.info("biz end.");
+    try {
+      log.info("do biz");
+      sleepSecond(3);
+    } finally {
+      lock.unlock();
     }
-
-    @Test
-    public void testRelease() throws Exception {
-        String lockKey = "lock1";
-        RLock lock = client.getLock(lockKey);
-        boolean flag = lock.tryLock();
-
-        if (!flag) {
-            log.warn("fail to get all lock");
-            return;
-        }
-
-        RLock lock2 = client.getLock(lockKey);
-        boolean flag2 = lock2.tryLock();
+    log.info("biz end.");
+  }
 
 
-        try {
-            log.info("do biz");
-            sleepSecond(3);
+  @Test
+  public void testBucketsPut() throws Exception {
+    RBuckets buckets = client.getBuckets();
 
-            lock2.unlock();
-        } finally {
-            lock.unlock();
-        }
-        log.info("biz end.");
-    }
+    Map<String, String> data = new HashMap<>();
+    data.put("test:1", "1");
+    data.put("test:2", "2");
+    buckets.trySet(data);
+  }
 
-    @Test
-    public void testInMultiThread() throws Exception {
+  @Test
+  public void testBuckets() throws Exception {
+    RBuckets buckets = client.getBuckets();
 
+    Map<String, String> map = buckets.get("test:1", "test:2", "test:3");
+    log.info("map={}", map);
 
-        runInMultiThread(() -> {
-            RLock lock = client.getLock("lock1");
-            boolean flag = false;
-            try {
-                flag = lock.tryLock(0, TimeUnit.SECONDS);
-            } catch (InterruptedException e) {
-                log.error("fail to interrupted exception", e);
-            }
-
-            if (!flag) {
-                log.warn("fail to get all lock");
-                return;
-            }
-
-            try {
-                log.info("do biz");
-                sleepSecond(3);
-            } finally {
-                lock.unlock();
-            }
-            log.info("biz end.");
-        });
-        sleepSecond(10);
-    }
-
-
-    @Test
-    public void testMultiLock() throws Exception {
-        RLock lock1 = client.getLock("lock1");
-        RLock lock2 = client.getLock("lock2");
-        RLock lock3 = client.getLock("lock3");
-
-        RedissonMultiLock lock = new RedissonMultiLock(lock1, lock2, lock3);
-
-        boolean flag = lock.tryLock(0, TimeUnit.SECONDS);
-
-        if (!flag) {
-            log.warn("fail to get all lock");
-            return;
-        }
-
-        try {
-            log.info("do biz");
-            sleepSecond(3);
-        } finally {
-            lock.unlock();
-        }
-        log.info("biz end.");
-    }
+  }
 
 }
