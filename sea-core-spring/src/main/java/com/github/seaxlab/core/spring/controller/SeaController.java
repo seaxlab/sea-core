c536c6c4b07c7c50380be751047b3c95d2037073
shipengyan <taotao365s@163.com>
2023-03-23 14:19:47 +0800

    bean any invoke

--- sea-core-spring/src/main/java/com/github/seaxlab/core/spring/controller/SeaController.java
+++ sea-core-spring/src/main/java/com/github/seaxlab/core/spring/controller/SeaController.java
@@ -1,16 +1,18 @@
 package com.github.seaxlab.core.spring.controller;
 
 import com.github.seaxlab.core.component.json.jackson.util.JacksonUtil;
+import com.github.seaxlab.core.exception.Precondition;
 import com.github.seaxlab.core.model.Result;
-import com.github.seaxlab.core.spring.component.invoke.dto.InvokeReqDTO;
 import com.github.seaxlab.core.util.ArrayUtil;
 import com.github.seaxlab.core.util.EqualUtil;
 import com.github.seaxlab.core.util.JSONUtil;
 import com.github.seaxlab.core.util.ReflectUtil;
 import com.github.seaxlab.core.util.StringUtil;
 import com.github.seaxlab.core.web.util.ResponseUtil;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.Map;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -44,38 +46,29 @@ public class SeaController {
   /**
    * invoke public method of any bean service
    *
-   * @param dto
+   * @param params params
    * @return
    */
   @PostMapping("/execute")
-  public Result execute(@RequestBody InvokeReqDTO dto) {
-    log.info("try to execute simple dto={}", dto);
-    Object obj = null;
-
+  public Result execute(@RequestBody Map<String, Object> params) {
+    log.info("try to execute, params={}", params);
+    Object obj;
     try {
-      Object bean = ctx.getBean(dto.getService());
-      Method[] methods = bean.getClass().getMethods();
-
-      if (ArrayUtil.isNotEmpty(methods) && StringUtil.isNotEmpty(dto.getArgument())) {
-        Method methodObj = Arrays.stream(methods) //
-          .filter(item -> EqualUtil.isEq(item.getName(), dto.getMethod())) //
-          .findFirst().get();
-
-        if (methodObj.getParameterCount() > 0) {
-          Class<?> parameterType = methodObj.getParameterTypes()[0];
-          if (JSONUtil.isSimpleValid(dto.getArgument())) {
-            obj = MethodUtils.invokeMethod(bean, dto.getMethod(),
-              JacksonUtil.toObject(dto.getArgument(), parameterType));
-          } else {
-            obj = MethodUtils.invokeMethod(bean, dto.getMethod(), dto.getArgument());
+      String argument = "";
+      Object argumentObj = params.get("argument");
+      if (argumentObj != null) {
+        if (argumentObj instanceof String) {
+          String value = (String) argumentObj;
+          if (StringUtil.isNotBlank(value)) {
+            argument = value;
           }
         } else {
-          obj = MethodUtils.invokeMethod(bean, dto.getMethod(), dto.getArgument());
+          argument = JacksonUtil.toString(argumentObj);
         }
-      } else {
-        obj = ReflectUtil.invokeMethod(bean, dto.getMethod());
       }
+      log.info("final argument={}", argument);
 
+      obj = invokeMethod((String) params.get("service"), (String) params.get("method"), argument);
       if (obj instanceof Result) {
         return (Result) obj;
       }
@@ -84,6 +77,38 @@ public class SeaController {
       return Result.failMsg(e.getMessage());
     }
     return Result.success(obj);
+  }
+
+
+  //-------------------------------------private
+  private Object invokeMethod(String service, String method, String argument)
+    throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+    Precondition.checkNotBlank(service, "service cannot be empty.");
+    Precondition.checkNotBlank(method, "method cannot be empty.");
+    //
+    Object obj;
+    Object bean = ctx.getBean(service);
+    Method[] methods = bean.getClass().getMethods();
+
+    if (ArrayUtil.isNotEmpty(methods) && StringUtil.isNotEmpty(argument)) {
+      Method methodObj = Arrays.stream(methods) //
+        .filter(item -> EqualUtil.isEq(item.getName(), method)) //
+        .findFirst().get();
+
+      if (methodObj.getParameterCount() > 0) {
+        Class<?> parameterType = methodObj.getParameterTypes()[0];
+        if (JSONUtil.isSimpleValid(argument)) {
+          obj = MethodUtils.invokeMethod(bean, method, JacksonUtil.toObject(argument, parameterType));
+        } else {
+          obj = MethodUtils.invokeMethod(bean, method, argument);
+        }
+      } else {
+        obj = MethodUtils.invokeMethod(bean, method);
+      }
+    } else {
+      obj = ReflectUtil.invokeMethod(bean, method);
+    }
+    return obj;
   }
 
 }
