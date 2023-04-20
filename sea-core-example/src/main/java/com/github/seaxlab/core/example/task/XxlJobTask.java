d6034e106d4bc349b1efeae54d9020651b6a242b
shipengyan <taotao365s@163.com>
2023-04-20 19:40:09 +0800

    xxl-job example

---
+++ sea-core-example/src/main/java/com/github/seaxlab/core/example/task/XxlJobTask.java
@@ -0,0 +1,25 @@
+package com.github.seaxlab.core.example.task;
+
+import com.xxl.job.core.handler.annotation.XxlJob;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * module name
+ *
+ * @author spy
+ * @version 1.0 2023/4/20
+ * @since 1.0
+ */
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class XxlJobTask {
+
+  @XxlJob("simple-xxl-job")
+  public void simpleXxlJob() {
+    log.info("simple xxl job execute");
+  }
+
+}
