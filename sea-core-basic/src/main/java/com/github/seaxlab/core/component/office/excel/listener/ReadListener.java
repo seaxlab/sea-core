e6444ccba3144607c04b99dd6c2be4857026e874
shipengyan <taotao365s@163.com>
2023-05-06 18:02:32 +0800

    excel

---
+++ sea-core-basic/src/main/java/com/github/seaxlab/core/component/office/excel/listener/ReadListener.java
@@ -0,0 +1,12 @@
+package com.github.seaxlab.core.component.office.excel.listener;
+
+/**
+ * read listener
+ *
+ * @author spy
+ * @version 1.0 2023/5/6
+ * @since 1.0
+ */
+public interface ReadListener<T> {
+
+}
