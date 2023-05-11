811faab72b5c9ff5e4380a8a8a1a28e022c73075
shipengyan <taotao365s@163.com>
2023-05-11 15:30:32 +0800

    clean

--- sea-core-spring/src/main/java/com/github/seaxlab/core/spring/tx/service/TxService.java
+++ sea-core-spring/src/main/java/com/github/seaxlab/core/spring/tx/service/TxService.java
@@ -4,7 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
- * module name
+ * Tx service
  *
  * @author spy
  * @version 1.0 2022/6/21
@@ -13,33 +13,34 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class TxService {
 
-  @Transactional(rollbackFor = Exception.class, timeout = 30)
+  @Transactional(rollbackFor = Exception.class)
   public <R> R execute(Functions.Func0<R> func) {
     return func.apply();
   }
 
-  @Transactional(rollbackFor = Exception.class, timeout = 30)
+  @Transactional(rollbackFor = Exception.class)
   public <T, R> R execute(Functions.Func1<T, R> func, T t) {
     return func.apply(t);
   }
 
-  @Transactional(rollbackFor = Exception.class, timeout = 30)
+  @Transactional(rollbackFor = Exception.class)
   public <T1, T2, R> R execute(Functions.Func2<T1, T2, R> func, T1 t1, T2 t2) {
     return func.apply(t1, t2);
   }
 
-  @Transactional(rollbackFor = Exception.class, timeout = 30)
+  @Transactional(rollbackFor = Exception.class)
   public <T1, T2, T3, R> R execute(Functions.Func3<T1, T2, T3, R> func, T1 t1, T2 t2, T3 t3) {
     return func.apply(t1, t2, t3);
   }
 
-  @Transactional(rollbackFor = Exception.class, timeout = 30)
+  @Transactional(rollbackFor = Exception.class)
   public <T1, T2, T3, T4, R> R execute(Functions.Func4<T1, T2, T3, T4, R> func, T1 t1, T2 t2, T3 t3, T4 t4) {
     return func.apply(t1, t2, t3, t4);
   }
 
-  @Transactional(rollbackFor = Exception.class, timeout = 30)
-  public <T1, T2, T3, T4, T5, R> R execute(Functions.Func5<T1, T2, T3, T4, T5, R> func, T1 t1, T2 t2, T3 t3, T4 t4, T5 t5) {
+  @Transactional(rollbackFor = Exception.class)
+  public <T1, T2, T3, T4, T5, R> R execute(Functions.Func5<T1, T2, T3, T4, T5, R> func, T1 t1, T2 t2, T3 t3, T4 t4,
+    T5 t5) {
     return func.apply(t1, t2, t3, t4, t5);
   }
 
