adc71273a316afcce8167895ffb69a952d1aa878
shipengyan <taotao365s@163.com>
2023-05-12 17:26:36 +0800

    encrypt/decrypt by segment in RSA

--- sea-core-basic/src/main/java/com/github/seaxlab/core/security/util/RSAUtil.java
+++ sea-core-basic/src/main/java/com/github/seaxlab/core/security/util/RSAUtil.java
@@ -3,20 +3,27 @@ package com.github.seaxlab.core.security.util;
 import com.github.seaxlab.core.exception.BaseAppException;
 import com.github.seaxlab.core.exception.ErrorMessageEnum;
 import com.github.seaxlab.core.model.Tuple2;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.Signature;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
 import lombok.extern.slf4j.Slf4j;
 import org.bouncycastle.util.encoders.Base64;
+import org.springframework.util.Base64Utils;
 
 /**
  * RSA util
@@ -40,7 +47,7 @@ public class RSAUtil {
   /**
    * 密钥大小
    */
-  public static final int KEY_SIZE = 1024;
+  public static final int KEY_SIZE = 2048;
 
   public static final String SHA_256_WITH_RSA = "SHA256WithRSA";
 
@@ -75,6 +82,36 @@ public class RSAUtil {
       log.error("fail to generate RSA key pair, keySize={}", keySize, e);
       throw new BaseAppException(ErrorMessageEnum.METHOD_EXECUTION_ERROR);
     }
+  }
+
+  /**
+   * 获取公钥对象
+   *
+   * @param publicKeyBase64
+   * @return PublicKey
+   * @throws InvalidKeySpecException
+   * @throws NoSuchAlgorithmException
+   */
+  public static PublicKey getPublicKey(String publicKeyBase64)
+    throws InvalidKeySpecException, NoSuchAlgorithmException {
+    KeyFactory keyFactory = KeyFactory.getInstance(RSA);
+    X509EncodedKeySpec keySpec = new X509EncodedKeySpec(Base64.decode(publicKeyBase64));
+    return keyFactory.generatePublic(keySpec);
+  }
+
+  /**
+   * 获取私钥对象
+   *
+   * @param privateKeyBase64
+   * @return PrivateKey
+   * @throws NoSuchAlgorithmException
+   * @throws InvalidKeySpecException
+   */
+  public static PrivateKey getPrivateKey(String privateKeyBase64)
+    throws NoSuchAlgorithmException, InvalidKeySpecException {
+    KeyFactory keyFactory = KeyFactory.getInstance(RSA);
+    PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(Base64.decode(privateKeyBase64));
+    return keyFactory.generatePrivate(keySpec);
   }
 
   //--------------------------------RSA签名 begin-----------------------------------------
@@ -179,6 +216,171 @@ public class RSAUtil {
     }
   }
   //--------------------------------RSA加密 end-----------------------------------------
+
+  // 部分段场景下会报错：Data must not be longer than 245 bytes
+
+  //-------------------------------RSA 分段加解密 begin------------------------------------------
+
+
+  /**
+   * 使用公钥加密
+   *
+   * @param content         待加密内容
+   * @param publicKeyBase64 公钥 base64 编码
+   * @return 经过 base64 编码后的字符串
+   */
+  public static String encryptBySegment(String publicKeyBase64, String content) {
+    return encryptBySegment(publicKeyBase64, content, KEY_SIZE / 8 - 11);
+  }
+
+  /**
+   * 使用公钥加密（分段加密）
+   *
+   * @param content         待加密内容
+   * @param publicKeyBase64 公钥 base64 编码
+   * @param segmentSize     分段大小,一般小于 keySize/8（段小于等于0时，将不使用分段加密）
+   * @return 经过 base64 编码后的字符串
+   */
+  public static String encryptBySegment(String publicKeyBase64, String content, int segmentSize) {
+    try {
+      PublicKey publicKey = getPublicKey(publicKeyBase64);
+      return encryptBySegment(publicKey, content, segmentSize);
+    } catch (Exception e) {
+      log.error("fail to encrypt by segment", e);
+      return null;
+    }
+  }
+
+  /**
+   * 分段加密
+   *
+   * @param ciphertext  密文
+   * @param key         加密秘钥
+   * @param segmentSize 分段大小，<=0 不分段
+   * @return
+   */
+  private static String encryptBySegment(java.security.Key key, String ciphertext, int segmentSize) {
+    try {
+      // 用公钥加密
+      byte[] srcBytes = ciphertext.getBytes();
+
+      // Cipher负责完成加密或解密工作，基于RSA
+      Cipher cipher = Cipher.getInstance(RSA);
+      // 根据公钥，对Cipher对象进行初始化
+      cipher.init(Cipher.ENCRYPT_MODE, key);
+      byte[] resultBytes = null;
+
+      if (segmentSize > 0) {
+        resultBytes = cipherDoFinal(cipher, srcBytes, segmentSize); //分段加密
+      } else {
+        resultBytes = cipher.doFinal(srcBytes);
+      }
+      String base64Str = Base64Utils.encodeToString(resultBytes);
+      return base64Str;
+    } catch (Exception e) {
+      log.error("fail to encrypt by segment", e);
+      return null;
+    }
+  }
+
+  /**
+   * 分段大小
+   *
+   * @param cipher
+   * @param srcBytes
+   * @param segmentSize
+   * @return
+   * @throws IllegalBlockSizeException
+   * @throws BadPaddingException
+   * @throws IOException
+   */
+  private static byte[] cipherDoFinal(Cipher cipher, byte[] srcBytes, int segmentSize)
+    throws IllegalBlockSizeException, BadPaddingException, IOException {
+    if (segmentSize <= 0) {
+      throw new RuntimeException("分段大小必须大于0");
+    }
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    int inputLen = srcBytes.length;
+    int offSet = 0;
+    byte[] cache;
+    int i = 0;
+    // 对数据分段解密
+    while (inputLen - offSet > 0) {
+      if (inputLen - offSet > segmentSize) {
+        cache = cipher.doFinal(srcBytes, offSet, segmentSize);
+      } else {
+        cache = cipher.doFinal(srcBytes, offSet, inputLen - offSet);
+      }
+      out.write(cache, 0, cache.length);
+      i++;
+      offSet = i * segmentSize;
+    }
+    byte[] data = out.toByteArray();
+    out.close();
+    return data;
+  }
+
+  /**
+   * 使用私钥解密
+   *
+   * @param contentBase64    待加密内容,base64 编码
+   * @param privateKeyBase64 私钥 base64 编码
+   * @return
+   * @segmentSize 分段大小
+   */
+  public static String decryptBySegment(String privateKeyBase64, String contentBase64) {
+    return decryptBySegment(privateKeyBase64, contentBase64, KEY_SIZE / 8);
+  }
+
+  /**
+   * 使用私钥解密（分段解密）
+   *
+   * @param contentBase64    待加密内容,base64 编码
+   * @param privateKeyBase64 私钥 base64 编码
+   * @return
+   * @segmentSize 分段大小
+   */
+  public static String decryptBySegment(String privateKeyBase64, String contentBase64, int segmentSize) {
+    try {
+      PrivateKey privateKey = getPrivateKey(privateKeyBase64);
+      return decryptBySegment(privateKey, contentBase64, segmentSize);
+    } catch (Exception e) {
+      log.error("fail to decrypt by segment", e);
+      return null;
+    }
+  }
+
+  /**
+   * 分段解密
+   *
+   * @param contentBase64 密文
+   * @param key           解密秘钥
+   * @param segmentSize   分段大小（小于等于0不分段）
+   * @return
+   */
+  private static String decryptBySegment(java.security.Key key, String contentBase64, int segmentSize) {
+    try {
+      // 用私钥解密
+      byte[] srcBytes = Base64Utils.decodeFromString(contentBase64);
+      // Cipher负责完成加密或解密工作，基于RSA
+      Cipher deCipher = Cipher.getInstance(RSA);
+      // 根据公钥，对Cipher对象进行初始化
+      deCipher.init(Cipher.DECRYPT_MODE, key);
+      byte[] decBytes = null;//deCipher.doFinal(srcBytes);
+      if (segmentSize > 0) {
+        decBytes = cipherDoFinal(deCipher, srcBytes, segmentSize); //分段加密
+      } else {
+        decBytes = deCipher.doFinal(srcBytes);
+      }
+
+      String decrytStr = new String(decBytes);
+      return decrytStr;
+    } catch (Exception e) {
+      log.error("fail to decrypt by segment", e);
+      return null;
+    }
+  }
+  //-------------------------------RSA 分段加解密 end------------------------------------------
 
 
 }
--- sea-core-basic/src/test/java/com/github/seaxlab/core/security/RSAUtilTest.java
+++ sea-core-basic/src/test/java/com/github/seaxlab/core/security/RSAUtilTest.java
@@ -3,6 +3,7 @@ package com.github.seaxlab.core.security;
 import com.github.seaxlab.core.BaseCoreTest;
 import com.github.seaxlab.core.model.Tuple2;
 import com.github.seaxlab.core.security.util.RSAUtil;
+import com.github.seaxlab.core.util.RandomUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 
@@ -15,6 +16,20 @@ import org.junit.Test;
  */
 @Slf4j
 public class RSAUtilTest extends BaseCoreTest {
+
+  @Test
+  public void testKeyPair() throws Exception {
+    Tuple2<String, String> tuple = RSAUtil.generateKeyPair();
+
+    System.out.println("公钥：" + tuple.getFirst());
+    System.out.println("私钥：" + tuple.getSecond());
+
+    System.out.println("---------------------");
+    tuple = RSAUtil.generateKeyPair(2048);
+
+    System.out.println("公钥：" + tuple.getFirst());
+    System.out.println("私钥：" + tuple.getSecond());
+  }
 
   @Test
   public void test1024() throws Exception {
@@ -41,6 +56,27 @@ public class RSAUtilTest extends BaseCoreTest {
     test2(tuple.getFirst(), tuple.getSecond(), content);
     System.out.println("\n");
   }
+
+
+  @Test
+  public void testEncrypt() {
+    String publicKey = getPassword("sea.rsa_public_key");
+    String content = "";
+    content += "\n" + RandomUtil.uuid();
+    System.out.println(RSAUtil.encryptBySegment(publicKey, content));
+
+    //
+  }
+
+  @Test
+  public void testDecrypt() throws Exception {
+    String privateKey = getPassword("sea.rsa_private_key");
+    String content = "IaW8tzMLobhVeb2cZCPzOiS7owXT12sLPLMhW8kBXXa+UiFlqXEXMt1alMcaAu9tq7PVSF1WSB8fyRoVjev7wM0FgAXxAKMx314usmt1GKGzfZAT24qsu0bvWFH7iobugEAWlMfvviRtbp2ECAxzFPL5UH8xJjMs0rfd1YbLEAG6huW/2QuR2/87Dl0zQU4tfeh4a4UjxfKC0SKl450gtWkn4GUz2J1CQAdxdV1Hb+h5W6FatePf/1b/iC4qjCYFratUCTsAfMO1BKd9ML9hpJr3FBTwk/YbeH9m1QKfleCpaych4BaEtSrMuR/WLJrAwFpfqWs5+hEL4woF1iC33T8h/hlekPhnn6RUyh3LccJP955GIVdeE6PSfWuBs6Sz3r//at8P+K0fOQfnrgyiGMn3pXfU07uHCExOuXrQCWaXFbFoqhEeHRliT42YPfMapK6o71cwS2aZN9hJtws7CvlBqCvYLYt7HssnwIlfHau6DmSAVN1ZQ7cha23jWqJ0IoI4dK0cM80rB0hNhXSm4/9620pIzlVDvO2AR+eJ9/6UTBnH+XQKri9FlBKCKR88t4leo2nV0rN+0sK/QpoU5NpzDkV4CNJURmwEd00qR8gh0NluHociDv0IKiK06PRHHrawwpjLEm1vphQugq/6Mzb2dXmSOKoPPU14xgXiVtZKuitJuaeu90QbvX1oQv39nSkhSR45WO/x/h3HN2YX2DLkdzuFDd92+jO/TvvKEhMgs9wWXJrZOB7ZSX5T4K/2qW3J5P5ruDoT3XuoPsn8Vhc7CbXQJbQHxaIz/8tA4+NEvFtHf/WVbfNvWENHg5JJwOnTH68A37sVU1bVXNlgwGQ4eHfU7VAaDnv4pZR6OrAZ1xuTF3TZwu6InVIxMXZZv8TS6y222gFrL31yLBIyp1KBYcKZv2XnST7W2Dt6TqL8PKYom84Ahqwfa85gWngn5bTNxYDZIHYG95BcB+AEgzuzVH100hk6mrleoE69oolcsIBy6qUekYJo0DrgzbLY";
+
+    System.out.println(RSAUtil.decryptBySegment(privateKey, content));
+  }
+
+  //-------------------------------------
 
   private static void test1(String publicKey, String privateKey, String source) throws Exception {
     System.out.println("***************** 公钥加密私钥解密开始 *****************");
