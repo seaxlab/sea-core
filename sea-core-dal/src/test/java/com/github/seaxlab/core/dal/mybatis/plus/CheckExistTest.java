bc2c594c5b7d954601cf9d6afab72b89a30e5a33
shipengyan <taotao365s@163.com>
2023-08-24 18:56:32 +0800

    tx test

--- sea-core-dal/src/test/java/com/github/seaxlab/core/dal/mybatis/plus/CheckExistTest.java
+++ sea-core-dal/src/test/java/com/github/seaxlab/core/dal/mybatis/plus/CheckExistTest.java
@@ -2,14 +2,9 @@ package com.github.seaxlab.core.dal.mybatis.plus;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
-import com.github.seaxlab.core.spring.tx.util.TxUtil;
-import com.github.seaxlab.core.util.IdUtil;
 import javax.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.transaction.TransactionStatus;
 
 /**
  * module name
@@ -43,35 +38,6 @@ public class CheckExistTest extends BasePlusTest {
     log.info("exist={}", exist);
   }
 
-  @Autowired
-  private DataSourceTransactionManager dataSourceTransactionManager;
 
-  @Resource
-  private User2Service user2Service;
-
-  @Test
-  public void testTx() throws Exception {
-    TransactionStatus txStatus = TxUtil.begin(dataSourceTransactionManager);
-    try {
-      User2 entity = new User2();
-      entity.setName("t_" + IdUtil.getYYYYMMDDHHMMSSSSS());
-      user2Mapper.insert(entity);
-      user2Service.add();
-      TxUtil.commit(dataSourceTransactionManager, txStatus);
-    } catch (Exception e) {
-      log.error("ee", e);
-      TxUtil.rollback(dataSourceTransactionManager, txStatus);
-    }
-  }
-
-  @Test
-  public void testTx2() throws Exception {
-    user2Service.add21();
-  }
-
-  @Test
-  public void testTx3() throws Exception {
-
-  }
 
 }
