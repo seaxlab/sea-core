b728a7301db493a9fc554ba7a807627e96c5d7c8
shipengyan <taotao365s@163.com>
2023-11-13 17:31:23 +0800

    po extra

---
+++ sea-core-model/src/main/java/com/github/seaxlab/core/model/layer/po/POExtra.java
@@ -0,0 +1,14 @@
+package com.github.seaxlab.core.model.layer.po;
+
+import java.io.Serializable;
+
+/**
+ * Po Extra
+ *
+ * @author spy
+ * @version 1.0 2023/11/13
+ * @since 1.0
+ */
+public class POExtra implements Serializable {
+
+}
