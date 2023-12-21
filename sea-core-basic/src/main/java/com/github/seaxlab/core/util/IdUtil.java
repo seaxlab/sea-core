f4148ee5b44006a53f6b18772d3b3ef07640e8e7
shipengyan <taotao365s@163.com>
2023-12-21 11:15:29 +0800

    refine project

--- sea-core-basic/src/main/java/com/github/seaxlab/core/util/IdUtil.java
+++ sea-core-basic/src/main/java/com/github/seaxlab/core/util/IdUtil.java
@@ -1,10 +1,9 @@
 package com.github.seaxlab.core.util;
 
 import com.github.seaxlab.core.enums.DateFormatEnum;
-import lombok.extern.slf4j.Slf4j;
-
 import java.util.Date;
 import java.util.UUID;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * unique key
@@ -69,7 +68,8 @@ public final class IdUtil {
   }
 
 
-  private static String[] chars = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+  private static final String[] chars = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+    "n",
     "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A",
     "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X",
     "Y", "Z"};
