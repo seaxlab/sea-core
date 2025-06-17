cc19b0b6aadf755ad2f0eff517a978cf16f1e5a7
shipengyan <taotao365s@163.com>
2025-06-17 16:01:31 +0800

    refine extension

--- sea-core-spring-boot-starter/src/main/java/com/github/seaxlab/core/boot/autoconfigure/SeaCoreExtensionAutoConfiguration.java
+++ sea-core-spring-boot-starter/src/main/java/com/github/seaxlab/core/boot/autoconfigure/SeaCoreExtensionAutoConfiguration.java
@@ -1,9 +1,9 @@
 package com.github.seaxlab.core.boot.autoconfigure;
 
-import com.github.seaxlab.core.spring.component.extension.register.ExtensionScanner;
 import com.github.seaxlab.core.spring.component.extension.executor.ExtensionExecutor;
 import com.github.seaxlab.core.spring.component.extension.register.ExtensionRegister;
 import com.github.seaxlab.core.spring.component.extension.register.ExtensionRepository;
+import com.github.seaxlab.core.spring.component.extension.register.ExtensionScanner;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,16 +17,22 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SeaCoreExtensionAutoConfiguration {
 
-  @Bean("seaCoreExtensionBootstrap")
-  @ConditionalOnMissingBean(ExtensionScanner.class)
-  public ExtensionScanner bootstrap() {
-    return new ExtensionScanner();
-  }
-
   @Bean("seaCoreExtensionRepository")
   @ConditionalOnMissingBean(ExtensionRepository.class)
   public ExtensionRepository repository() {
     return new ExtensionRepository();
+  }
+
+  @Bean("seaCoreExtensionRegister")
+  @ConditionalOnMissingBean(ExtensionRegister.class)
+  public ExtensionRegister register() {
+    return new ExtensionRegister();
+  }
+
+  @Bean("seaCoreExtensionScanner")
+  @ConditionalOnMissingBean(ExtensionScanner.class)
+  public ExtensionScanner bootstrap() {
+    return new ExtensionScanner();
   }
 
   @Bean("seaCoreExtensionExecutor")
@@ -35,10 +41,5 @@ public class SeaCoreExtensionAutoConfiguration {
     return new ExtensionExecutor();
   }
 
-  @Bean("seaCoreExtensionRegister")
-  @ConditionalOnMissingBean(ExtensionRegister.class)
-  public ExtensionRegister register() {
-    return new ExtensionRegister();
-  }
 
 }
